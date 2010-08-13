741c57f0cb7f860d28c6aee8e3aeee462418843f
Earl Miles <merlin@logrus.com>
2010-08-13 21:45:25 +0000

    #785960 by dereine: Reset button should appear after Apply button, not before.

--- theme/views-exposed-form.tpl.php
+++ theme/views-exposed-form.tpl.php
@@ -64,13 +64,13 @@
         <?php print $offset; ?>
       </div>
     <?php endif; ?>
+    <div class="views-exposed-widget views-submit-button">
+      <?php print $button ?>
+    </div>
     <?php if (!empty($reset_button)): ?>
-      <div class="views-exposed-widget">
+      <div class="views-exposed-widget views-reset-button">
         <?php print $reset_button; ?>
       </div>
     <?php endif; ?>
-    <div class="views-exposed-widget">
-      <?php print $button ?>
-    </div>
   </div>
 </div>
