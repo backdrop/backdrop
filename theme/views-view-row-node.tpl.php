f2a09eebc770c39e7d6b0ae98f370cdf610e15f8
Daniel Wehner <daniel.wehner@erdfisch.de>
2010-09-15 09:01:01 +0000

    #911148 by JeremyFrench: Help out of date for template suggestions.

--- theme/views-view-row-node.tpl.php
+++ theme/views-view-row-node.tpl.php
@@ -6,7 +6,7 @@
  *
  * Rather than doing anything with this particular template, it is more
  * efficient to use a variant of the node.tpl.php based upon the view,
- * which will be named node-view-VIEWNAME.tpl.php. This isn't actually
+ * which will be named node--view--VIEWNAME.tpl.php. This isn't actually
  * a views template, which is why it's not used here, but is a template
  * 'suggestion' given to the node template, and is used exactly
  * the same as any other variant of the node template file, such as
