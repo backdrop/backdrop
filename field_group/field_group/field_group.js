5cf8c8c9ba07da5a269caa90430eb61ba1f8a21e
Jochen Stals <stalski@322618.no-reply.drupal.org>
2010-10-22 17:29:48 +0000

    Added js code for accordion visibility toggle changed the js code for the collapsible div Added accordion to the field group format hooks

--- field_group/field_group/field_group.js
+++ field_group/field_group/field_group.js
@@ -3,41 +3,33 @@
 (function($) {
 
 /**
- * Toggle the visibility of a fieldset using smooth animations.
+ * Drupal FieldGroup object.
  */
-Drupal.toggleVis = function (element, baseClass) {
-  var $element = $(element);
-  if ($element.is('.collapsed')) {
-    var $content = $('> .'+ baseClass +'-wrapper', element).hide();
-    $element
-      .removeClass('collapsed')
-      .trigger({ type: 'collapsed', value: false })
-    $content.slideDown({
-      duration: 'fast',
-      easing: 'linear',
-      complete: function () {
-        Drupal.collapseScrollIntoView(element);
-        element.animating = false;
-      },
-      step: function () {
-        // Scroll the element into view.
-        Drupal.collapseScrollIntoView(element);
-      }
-    });
-  }
-  else {
-    $element.trigger({ type: 'collapsed', value: true });
-    $('> .'+ baseClass +'-wrapper', element).slideUp('fast', function () {
-      $element
-        .addClass('collapsed')
-      element.animating = false;
-    });
-  }
+Drupal.FieldGroup = Drupal.FieldGroup || {};
+
+/**
+ * Create a link to animate on.
+ */
+Drupal.FieldGroup.createLink = function (element) {
+  // Turn the legend into a clickable link, but retain span.field-group-format-toggler
+  // for CSS positioning.
+  var $toggler = $('span.field-group-format-toggler', element);
+  $('<span class="field-group-format-toggler element-invisible"></span>')
+    .prependTo($toggler)
+    .after(' ');
+  var $link = $('<a class="field-group-format-title" href="#"></a>');
+  $link.prepend($toggler.contents())
+    .appendTo($toggler);
+  return $link;
 };
-	
+
+/**
+ * Behaviors.
+ */
 Drupal.behaviors.fieldGroup = {
   attach: function (context, settings) {
     $('div.collapsible', context).once('togglevis', function () {
+    	
       var $wrapper = $(this);
       // Expand wrapper if there are errors inside, or if it contains an
       // element that is targeted by the uri fragment identifier. 
@@ -45,27 +37,25 @@ Drupal.behaviors.fieldGroup = {
       if ($('.error' + anchor, $wrapper).length) {
         $wrapper.removeClass('collapsed');
       }
-
-      // Turn the legend into a clickable link, but retain span.field-group-format-toggler
-      // for CSS positioning.
-      var $toggler = $('span.field-group-format-toggler', this);
-      $('<span class="field-group-format-toggler element-invisible"></span>')
-        .prependTo($toggler)
-        .after(' ');
+      
+      var $link = Drupal.FieldGroup.createLink(this);
 
       // .wrapInner() does not retain bound events.
-      var $link = $('<a class="field-group-format-title" href="#"></a>')
-        .prepend($toggler.contents())
-        .appendTo($toggler)
-        .click(function () {
+      $link.click(function () {
           var wrapper = $wrapper.get(0);
           // Don't animate multiple times.
           if (!wrapper.animating) {
             wrapper.animating = true;
-            Drupal.toggleVis(wrapper, 'field-group-format');
+            $('> .'+ baseClass +'-wrapper', element).toggle('blind', {}, 500);
+            element.animating = false;
           }
           return false;
         });
+    });
+    
+	var accordionWrapper = $('h3.accordion', context).parent();
+	accordionWrapper.once('accordion', function () {
+      $(this).accordion({ autoHeight: false });
     });
   }
 };
