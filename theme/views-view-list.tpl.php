5ed421159a76a0815baac3aeb97c147e478edce9
Katherine Senzee <katherine.senzee@acquia.com>
2010-11-10 16:38:19 -0800

    Initial commit of CVS checkout.

---
+++ theme/views-view-list.tpl.php
@@ -0,0 +1,21 @@
+<?php
+// $Id: views-view-list.tpl.php,v 1.3.6.1 2010/03/29 20:05:38 dereine Exp $
+/**
+ * @file views-view-list.tpl.php
+ * Default simple view template to display a list of rows.
+ *
+ * - $title : The title of this group of rows.  May be empty.
+ * - $options['type'] will either be ul or ol.
+ * @ingroup views_templates
+ */
+?>
+<div class="item-list">
+  <?php if (!empty($title)) : ?>
+    <h3><?php print $title; ?></h3>
+  <?php endif; ?>
+  <<?php print $options['type']; ?>>
+    <?php foreach ($rows as $id => $row): ?>
+      <li class="<?php print $classes_array[$id]; ?>"><?php print $row; ?></li>
+    <?php endforeach; ?>
+  </<?php print $options['type']; ?>>
+</div>
