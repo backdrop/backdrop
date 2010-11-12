040dfd93f755194081b0aef3910d5637984bc82f
Katherine Senzee <katherine.senzee@acquia.com>
2010-11-12 14:27:39 -0800

    Scaffold edit page using build arrays.

---
+++ js/views-admin.js
@@ -0,0 +1,7 @@
+// $Id$
+
+Drupal.behaviors.viewsUiEditView = {};
+
+Drupal.behaviors.viewsUiEditView.attach = function (context, settings) {
+  jQuery('.views-displays').once('views-ui-edit-view').tabs();
+};
---
+++ theme/views-ui-edit-display-tab.tpl.php
@@ -0,0 +1,39 @@
+<?php
+// $Id$
+/**
+ * @file views-ui-edit-display-tab.tpl.php
+ * Template for the primary view display editing window.
+ */
+?>
+<div class="views-display views-display-<?php print $id; ?>">
+  <p>Display: <?php print $display_id; ?></p>
+  
+  <?php // top section ?>
+  <div class="top">
+    <div class="inside">
+      Top section - do we still need this?
+    </div>
+  </div>
+
+  <?php // left section ?>
+  <div class="left tab-section">
+    <div class="inside">
+      Left section
+    </div>
+  </div>
+
+  <?php // middle section ?>
+  <div class="middle tab-section">
+    <div class="inside">
+      Middle section
+    </div>
+  </div>
+
+  <?php // right section ?>
+  <div class="right tab-section">
+    <div class="inside">
+      Right section
+    </div>
+  </div>
+
+</div>
