d6bea670aa3e76dc1a90ff59dad903226c740b97
pwolanin <pwolanin@gmail.com>
2010-11-19 13:59:13 -0500

    Restructure the form building/validate/submit into the class

--- plugins/views_wizard/views_ui_base_views_wizard.class.php
+++ plugins/views_wizard/views_ui_base_views_wizard.class.php
@@ -1,36 +1,33 @@
 <?php
 
 interface ViewsWizardInterface {
-  __construct($base_table, $values = array());
+  function __construct($plugin);
 
   /**
    * For AJAX callbacks to build other elements in the "show" form.
    */
-  function show_form_elements($form_build_id);
+  function build_form($form, &$form_state);
 
   /**
-   * For AJAX callbacks to build other elements in the "display_format" form.
-   */
-  function display_format_form_elements($form_build_id);
-
-  /**
-   * Validate values sent into the constructor.
-   */
-  function validate();
-
-  /**
-   * Create a new View from values sent into the constructor.
+   * Validate form and values.
    *
-   * @return a redirect path.
+   * @return an array of form errors.
    */
-  function create_view();
+  function validate($form, &$form_state);
 
   /**
-   * Store a new View from values sent into the constructor.
+   * Create a new View from form values.
    *
-   * @return a redirect path.
+   * @return a view object.
    */
-  function store_view();  
+  function create_view($form, &$form_state);
+
+  /**
+   * Store a new View from form values.
+   *
+   * @return a view object.
+   */
+  function store_view($form, &$form_state);  
 }
 
 /**
@@ -51,7 +48,7 @@ class ViewsUiBaseViewsWizard implements ViewsWizardInterface {
     'relationship' => 'none',
   );
 
-  __construct($plugin, $values = array()) {
+  function __construct($plugin) {
     $this->base_table = $plugin['base_table'];
     $default = $this->filter_defaults;
 
@@ -62,29 +59,176 @@ class ViewsUiBaseViewsWizard implements ViewsWizardInterface {
     $this->plugin = $plugin;
   }
 
-  show_form_elements($form_build_id) {
-    return '';
-  }
-
-  function display_format_form_elements($form_build_id) {
-    return '';
+  function build_form($form, &$form_state) {
+    $form['page'] = array(
+      '#type' => 'fieldset',
+      '#tree' => TRUE,
+    );
+    $form['page']['create'] = array(
+      '#title' => t('Create a page'),
+      '#type' => 'checkbox',
+      '#attributes' => array('class' => array('strong')),
+    );
+    $form['page']['title'] = array(
+      '#title' => t('Page title'),
+      '#type' => 'textfield',
+      '#states' => array(
+        'visible' => array(
+          ':input[name="page[create]"]' => array('checked' => TRUE),
+        ),
+      ),
+    );
+    $form['page']['path'] = array(
+      '#title' => t('Path'),
+      '#type' => 'textfield',
+      '#states' => array(
+        'visible' => array(
+          ':input[name="page[create]"]' => array('checked' => TRUE),
+        ),
+      ),
+    );
+    // This may change by AJAX as we change the base table of the selected wizard.
+    $form['page']['display_format']['style'] = array(
+      '#title' => t('Display format'),
+      '#help_topic' => 'style',
+      '#type' => 'select',
+      '#options' => views_fetch_plugin_names('style', 'normal', array($this->base_table)),
+      '#default_value' => 'default',
+      '#states' => array(
+        'visible' => array(
+          ':input[name="page[create]"]' => array('checked' => TRUE),
+        ),
+      ),
+    );
+    $form['page']['items_per_page'] = array(
+      '#title' => t('Items per page'),
+      '#type' => 'textfield',
+      '#size' => 5,
+      '#states' => array(
+        'visible' => array(
+          ':input[name="page[create]"]' => array('checked' => TRUE),
+        ),
+      ),
+    );
+    $form['page']['link'] = array(
+      '#title' => t('Create a menu link'),
+      '#type' => 'checkbox',
+      '#states' => array(
+        'visible' => array(
+          ':input[name="page[create]"]' => array('checked' => TRUE),
+        ),
+      ),
+    );
+    $form['page']['link_properties'] = array();
+    if (module_exists('menu')) {
+      $menu_options = menu_get_menus();
+    }
+    else {
+      // These are not yet translated.
+      $menu_options = menu_list_system_menus();
+      foreach ($menu_options as $name => $title) {
+        $menu_options[$name] = t($title);
+      }
+    }
+    $form['page']['link_properties']['menu_name'] = array(
+      '#title' => t('Menu'),
+      '#type' => 'select',
+      '#options' => $menu_options,
+      '#states' => array(
+        'visible' => array(
+        ':input[name="page[link]"]' => array('checked' => TRUE),
+        ':input[name="page[create]"]' => array('checked' => TRUE),
+        ),
+      ),
+    );
+    $form['page']['link_properties']['title'] = array(
+      '#title' => t('Link text'),
+      '#type' => 'textfield',
+      '#states' => array(
+        'visible' => array(
+        ':input[name="page[link]"]' => array('checked' => TRUE),
+        ':input[name="page[create]"]' => array('checked' => TRUE),
+        ),
+      ),
+    );
+    $form['page']['rss'] = array(
+      '#title' => t('Include an RSS feed'),
+      '#type' => 'checkbox',
+      '#states' => array(
+        'visible' => array(
+          ':input[name="page[create]"]' => array('checked' => TRUE),
+        ),
+      ),
+    );
+    $form['page']['rss_properties']['path'] = array(
+      '#title' => t('RSS path'),
+      '#type' => 'textfield',
+      '#states' => array(
+        'visible' => array(
+          ':input[name="page[create]"]' => array('checked' => TRUE),
+          ':input[name="page[rss]"]' => array('checked' => TRUE),
+        ),
+      ),
+    );
+    $form['block'] = array(
+      '#type' => 'fieldset',
+      '#tree' => TRUE,
+    );
+    $form['block']['create'] = array(
+      '#title' => t('Create a block'),
+      '#type' => 'checkbox',
+      '#attributes' => array('class' => array('strong')),
+    );
+    $form['block']['title'] = array(
+      '#title' => t('Block title'),
+      '#type' => 'textfield',
+      '#states' => array(
+        'visible' => array(
+          ':input[name="block[create]"]' => array('checked' => TRUE),
+        ),
+      ),
+    );
+    // This may change by AJAX as we change the base table of the selected wizard.
+    $form['block']['display_format']['style'] = array(
+      '#title' => t('Display format'),
+      '#help_topic' => 'style',
+      '#type' => 'select',
+      '#options' => views_fetch_plugin_names('style', 'normal', array($this->base_table)),
+      '#default_value' => 'default',
+      '#states' => array(
+        'visible' => array(
+          ':input[name="block[create]"]' => array('checked' => TRUE),
+        ),
+      ),
+    );
+    $form['block']['items_per_page'] = array(
+      '#title' => t('Items per page'),
+      '#type' => 'textfield',
+      '#size' => 5,
+      '#states' => array(
+        'visible' => array(
+          ':input[name="block[create]"]' => array('checked' => TRUE),
+        ),
+      ),
+    );
+    return $form;
   }
 
   /**
    * Validate values.
    */
-  function validate() {
-    return TRUE;
+  function validate($from, &$form_state) {
+    return array();
   }
 
   /**
    * Create a View from values.
    */
- function create_view() {
-   return '';
+ function create_view($from, &$form_state) {
+   return NULL;
  }
 
- function store_view() {
-   return '';
+ function store_view($from, &$form_state) {
+   return NULL;
  }
 }
