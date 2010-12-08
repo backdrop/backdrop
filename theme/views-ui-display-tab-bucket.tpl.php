e0695ad0d5fc7772d573923f32c3b5ed5a26b851
Katherine Senzee <katherine.senzee@acquia.com>
2010-12-07 17:41:29 -0800

    Rename 'section' to 'bucket' and add more of the existing buckets back in.

---
+++ theme/views-ui-display-tab-bucket.tpl.php
@@ -0,0 +1,14 @@
+<?php
+// $Id$
+/**
+ * @file views-ui-display-tab-bucket.tpl.php
+ * Template for each "box" on the display query edit screen.
+ */
+?>
+<div class="views-display-bucket">
+  <h3><?php print $title; ?></h3>
+  <?php print $rearrange; ?>
+  <?php print $add; ?>
+  <?php print $fields; ?>
+  <?php print $content; ?>
+</div>
