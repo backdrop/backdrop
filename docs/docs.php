cace21e459935d5d8d6fccdac954838880b8db5e
Earl Miles <merlin@logrus.com>
2010-12-17 21:13:16 +0000

    #627378 followup by dereine: documentation.

--- docs/docs.php
+++ docs/docs.php
@@ -270,6 +270,9 @@ function hook_views_plugins_alter(&$plugins) {
  *   - path: (optional) If includes are stored somewhere other than within
  *       the root module directory or a subdirectory called includes, specify
  *       its path here.
+ *   - template path: (optional) A path where the module has stored it's views template files.
+ *        When you have specificed this key views automatically uses the template files for the views.
+ *        You can use the same naming conventions like for normal views template files.
  */
 function hook_views_api() {
   return array(
