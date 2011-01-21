9997fa4fd1ea1da7b0460793ee51a3cfca21450b
Katherine Senzee <katherine.senzee@acquia.com>
2011-01-21 13:13:11 -0800

    Add a title field to node displays so they don't break when the user switches to a field-based row style.

--- plugins/views_wizard/views_ui_node_views_wizard.class.php
+++ plugins/views_wizard/views_ui_node_views_wizard.class.php
@@ -15,6 +15,24 @@ class ViewsUiNodeViewsWizard extends ViewsUiBaseViewsWizard {
     $display_options['row_plugin'] = 'node';
     $display_options['row_options']['links'] = 1;
 
+    // Add the title field, so that the display has content if the user switches
+    // to a row style that uses fields.
+    /* Field: Node: Title */
+    $display_options['fields']['title']['id'] = 'title';
+    $display_options['fields']['title']['table'] = 'node';
+    $display_options['fields']['title']['field'] = 'title';
+    $display_options['fields']['title']['alter']['alter_text'] = 0;
+    $display_options['fields']['title']['alter']['make_link'] = 0;
+    $display_options['fields']['title']['alter']['absolute'] = 0;
+    $display_options['fields']['title']['alter']['trim'] = 0;
+    $display_options['fields']['title']['alter']['word_boundary'] = 0;
+    $display_options['fields']['title']['alter']['ellipsis'] = 0;
+    $display_options['fields']['title']['alter']['strip_tags'] = 0;
+    $display_options['fields']['title']['alter']['html'] = 0;
+    $display_options['fields']['title']['hide_empty'] = 0;
+    $display_options['fields']['title']['empty_zero'] = 0;
+    $display_options['fields']['title']['link_to_node'] = 1;
+
     return $display_options;
   }
 }
