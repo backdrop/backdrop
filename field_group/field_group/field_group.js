b36e7e44ef76cba5f20327b9a2f46fae0c7d513d
Jochen Stals <stalski@322618.no-reply.drupal.org>
2011-01-28 22:24:41 +0000

    Moved asterix requirement to javascript made sure the fieldgroup settings are displayed underneath

--- field_group/field_group/field_group.js
+++ field_group/field_group/field_group.js
@@ -12,11 +12,19 @@ Drupal.FieldGroup.Effects = Drupal.FieldGroup.Effects || {};
  * Implements Drupal.FieldGroup.processHook().
  */
 Drupal.FieldGroup.Effects.processAccordion = {
-  execute: function (context, settings) {
+  execute: function (context, settings, type) {
     $('div.field-group-accordion-wrapper', context).accordion({
       autoHeight: false,
       active: '.field-group-accordion-active'
     });
+    if (type == 'form') {
+      // Add required fields mark to any element containing required fields
+      $('div.accordion-item').each(function(i){
+        if ($(this).find('.form-required').length > 0) {
+          $('h3.ui-accordion-header').eq(i).append('&nbsp;').append($('.form-required').eq(0).clone());
+        }
+      });
+    }
   }
 }
 
@@ -24,7 +32,31 @@ Drupal.FieldGroup.Effects.processAccordion = {
  * Implements Drupal.FieldGroup.processHook().
  */
 Drupal.FieldGroup.Effects.processHtabs = {
-  execute: function (context, settings) {
+  execute: function (context, settings, type) {
+    if (type == 'form') {
+      // Add required fields mark to any element containing required fields
+      $('fieldset.horizontal-tabs-pane').each(function(i){
+        if ($(this).find('.form-required').length > 0) {
+          $('li.horizontal-tab-button').eq(i).children('a').find('strong:first').after($('.form-required').eq(0).clone()).after('&nbsp;');
+        }
+      });
+    }
+  }
+}
+
+/**
+ * Implements Drupal.FieldGroup.processHook().
+ */
+Drupal.FieldGroup.Effects.processTabs = {
+  execute: function (context, settings, type) {
+    if (type == 'form') {
+      // Add required fields mark to any fieldsets containing required fields
+      $('fieldset.vertical-tabs-pane').each(function(i){
+        if ($(this).find('.form-required').length > 0) {
+          $('li.vertical-tab-button').eq(i).children('a').find('strong:first').after($('.form-required').eq(0).clone()).after('&nbsp;');
+        }
+      });
+    }
   }
 }
 
@@ -35,7 +67,7 @@ Drupal.FieldGroup.Effects.processHtabs = {
  *      necessary.
  */
 Drupal.FieldGroup.Effects.processDiv = {
-  execute: function (context, settings) {
+  execute: function (context, settings, type) {
 
     $('div.collapsible', context).each(function() {
       var $wrapper = $(this);
@@ -84,8 +116,9 @@ Drupal.behaviors.fieldGroup = {
       $.each(Drupal.FieldGroup.Effects, function (func) {
         // We check for a wrapper function in Drupal.field_group as 
         // alternative for dynamic string function calls.
-        if (settings.field_group[func.toLowerCase().replace("process", "")] != undefined && $.isFunction(this.execute)) {
-          this.execute(context, settings);
+        var type = func.toLowerCase().replace("process", "");
+        if (settings.field_group[type] != undefined && $.isFunction(this.execute)) {
+          this.execute(context, settings, settings.field_group[type]);
         }
       });
     });
