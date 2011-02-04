85121cd5c8c96ac88dea502ae79cdb67f041f114
Earl Miles <merlin@logrus.com>
2011-02-04 22:36:27 +0000

    #1035168 by Ao: Fix strict warning in unread node filter.

--- js/ajax.js
+++ js/ajax.js
@@ -43,7 +43,7 @@
     // Update the preview widget to preview the new tab.
     var display_id = id.replace('#views-tab-', '');
     $("#preview-display-id").append('<option selected="selected" value="' + display_id + '">' + response.title + '</option>');
- 
+
     Drupal.attachBehaviors(id);
     var instance = $.viewsUi.tabs.instances[$('#views-tabset').get(0).UI_TABS_UUID];
     $('#views-tabset').viewsClickTab(instance.$tabs.length);
