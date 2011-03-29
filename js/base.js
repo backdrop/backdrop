5e68aa75d32cd0dfbe6a227bef9cc55cbe6557d1
Earl Miles <merlin@logrus.com>
2011-03-29 11:40:52 -0700

    Issue #1090270 by ygerasimov: URI decode on query variables to ensure query variables with non-standard characters work.

--- js/base.js
+++ js/base.js
@@ -54,7 +54,7 @@ Drupal.Views.parseQueryString = function (query) {
       var pair = pairs[i].split('=');
       // Ignore the 'q' path argument, if present.
       if (pair[0] != 'q' && pair[1]) {
-        args[pair[0]] = decodeURIComponent(pair[1].replace(/\+/g, ' '));
+        args[decodeURIComponent(pair[0].replace(/\+/g, ' '))] = decodeURIComponent(pair[1].replace(/\+/g, ' '));
       }
     }
   }
