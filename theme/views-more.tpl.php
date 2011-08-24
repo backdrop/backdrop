0a9f6a19c5aac5ffc8bd78ebe3e9f2a6b13a4e60
Daniel Wehner <daniel.wehner@erdfisch.de>
2011-08-24 23:09:57 +0100

    #1259056 by stevector: Add view to views-more.tpl.php  and document the availible variables

--- theme/views-more.tpl.php
+++ theme/views-more.tpl.php
@@ -3,7 +3,9 @@
  * @file views-more.tpl.php
  * Theme the more link
  *
- * - $more_url: the url for the more link
+ * - $view: The view object.
+ * - $more_url: the url for the more link.
+ * - $link_text: the text for the more link.
  *
  * @ingroup views_templates
  */
