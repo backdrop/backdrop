5c84f2af7b90e8004dd59724d48906516245dbd7
Daniel Wehner <daniel.wehner@erdfisch.de>
2011-10-18 22:27:12 +0200

    #1294636 by dereine: objective ajax-view.js

--- js/ajax_view.js
+++ js/ajax_view.js
@@ -11,72 +11,97 @@
 Drupal.behaviors.ViewsAjaxView = {};
 Drupal.behaviors.ViewsAjaxView.attach = function() {
   if (Drupal.settings && Drupal.settings.views && Drupal.settings.views.ajaxViews) {
-    // Retrieve the path to use for views' ajax.
-    var ajax_path = Drupal.settings.views.ajax_path;
-
-    // If there are multiple views this might've ended up showing up multiple times.
-    if (ajax_path.constructor.toString().indexOf("Array") != -1) {
-      ajax_path = ajax_path[0];
-    }
-
     $.each(Drupal.settings.views.ajaxViews, function(i, settings) {
-      var view = '.view-dom-id-' + settings.view_dom_id;
-      var element_settings = {
-        url: ajax_path,
-        submit: settings,
-        setClick: true,
-        event: 'click',
-        selector: view,
-        progress: { type: 'throbber' }
-      };
-
-      // Process exposed filter forms.
-      $('form#views-exposed-form-' + settings.view_name.replace(/_/g, '-') + '-' + settings.view_display_id.replace(/_/g, '-'))
-      .filter(':not(.views-processed)')
-      .each(function () {
-        var button = $('input[type=submit], input[type=image]', this);
-        button = button[0];
-
-        var ajax = new Drupal.ajax($(button).attr('id'), button, element_settings);
-      })
-      .addClass('views-processed')
-
-      $(view).filter(':not(.views-processed)')
-        // Don't attach to nested views. Doing so would attach multiple behaviors
-        // to a given element.
-        .filter(function() {
-          // If there is at least one parent with a view class, this view
-          // is nested (e.g., an attachment). Bail.
-          return !$(this).parents('.view').size();
-        })
-        .each(function() {
-          // Set a reference that will work in subsequent calls.
-          var target = this;
-          $(this)
-            .addClass('views-processed')
-            // Process pager, tablesort, and attachment summary links.
-            .find('ul.pager > li > a, th.views-field a, .attachment .views-summary a')
-            .each(function () {
-              var viewData = {};
-              // Construct an object using the settings defaults and then overriding
-              // with data specific to the link.
-              $.extend(
-                viewData,
-                settings,
-                Drupal.Views.parseQueryString($(this).attr('href')),
-                // Extract argument data from the URL.
-                Drupal.Views.parseViewArgs($(this).attr('href'), settings.view_base_path)
-              );
-
-              // For anchor tags, these will go to the target of the anchor rather
-              // than the usual location.
-              $.extend(viewData, Drupal.Views.parseViewArgs($(this).attr('href'), settings.view_base_path));
-
-              element_settings.submit = viewData;
-              var ajax = new Drupal.ajax(false, this, element_settings);
-            }); // .each function () {
-      }); // $view.filter().each
-    }); // .each Drupal.settings.views.ajaxViews
-  } // if
+      // @todo: Figure out where to store the object.
+      new Drupal.views.ajaxView(settings);
+    });
+  }
 };
+
+Drupal.views = {};
+
+/**
+ * Javascript object for a certain view.
+ */
+Drupal.views.ajaxView = function(settings) {
+  var selector = '.view-dom-id-' + settings.view_dom_id;
+  this.$view = $(selector);
+
+  // Retrieve the path to use for views' ajax.
+  var ajax_path = Drupal.settings.views.ajax_path;
+
+  // If there are multiple views this might've ended up showing up multiple times.
+  if (ajax_path.constructor.toString().indexOf("Array") != -1) {
+    ajax_path = ajax_path[0];
+  }
+
+  this.element_settings = {
+    url: ajax_path,
+    submit: settings,
+    setClick: true,
+    event: 'click',
+    selector: selector,
+    progress: { type: 'throbber' }
+  };
+
+  this.settings = settings;
+
+  // Add the ajax to exposed forms.
+  this.$exposed_form = $('form#views-exposed-form-'+ settings.view_name.replace(/_/g, '-') + '-' + settings.view_display_id.replace(/_/g, '-'));
+  this.$exposed_form.once(jQuery.proxy(this.attachExposedFormAjax, this));
+
+  // Add the ajax to pagers.
+  this.$view
+    // Don't attach to nested views. Doing so would attach multiple behaviors
+    // to a given element.
+    .filter(jQuery.proxy(this.filterNestedViews, this))
+    .once(jQuery.proxy(this.attachPagerAjax, this));
+};
+
+Drupal.views.ajaxView.prototype.attachExposedFormAjax = function() {
+  var button = $('input[type=submit], input[type=image]', this.$exposed_form);
+  button = button[0];
+
+  this.exposedFormAjax = new Drupal.ajax($(button).attr('id'), button, this.element_settings);
+};
+
+Drupal.views.ajaxView.prototype.filterNestedViews= function() {
+  // If there is at least one parent with a view class, this view
+  // is nested (e.g., an attachment). Bail.
+  return !this.$view.parents('.view').size();
+};
+
+/**
+ * Attach the ajax behavior to each link.
+ */
+Drupal.views.ajaxView.prototype.attachPagerAjax = function() {
+  this.$view.find('ul.pager > li > a, th.views-field a, .attachment .views-summary a')
+  .each(jQuery.proxy(this.attachPagerLinkAjax, this));
+};
+
+/**
+ * Attach the ajax behavior to a singe link.
+ */
+Drupal.views.ajaxView.prototype.attachPagerLinkAjax = function(id, link) {
+  var $link = $(link);
+  var viewData = {};
+  var href = $link.attr('href');
+  // Construct an object using the settings defaults and then overriding
+  // with data specific to the link.
+  $.extend(
+    viewData,
+    this.settings,
+    Drupal.Views.parseQueryString(href),
+    // Extract argument data from the URL.
+    Drupal.Views.parseViewArgs(href, this.settings.view_base_path)
+  );
+
+  // For anchor tags, these will go to the target of the anchor rather
+  // than the usual location.
+  $.extend(viewData, Drupal.Views.parseViewArgs(href, this.settings.view_base_path));
+
+  this.element_settings.submit = viewData;
+  this.pagerAjax = new Drupal.ajax(false, $link, this.element_settings);
+};
+
 })(jQuery);
