f18e442e2530d619f35f4cc0f507f44246199068
Daniel Wehner <daniel.wehner@erdfisch.de>
2011-11-26 21:21:52 +0100

    #1233334 by stborchert: Readd scrolling to the top js commando.

--- js/ajax_view.js
+++ js/ajax_view.js
@@ -104,4 +104,23 @@ Drupal.views.ajaxView.prototype.attachPagerLinkAjax = function(id, link) {
   this.pagerAjax = new Drupal.ajax(false, $link, this.element_settings);
 };
 
+Drupal.ajax.prototype.commands.viewsScrollTop = function (ajax, response, status) {
+  // Scroll to the top of the view. This will allow users
+  // to browse newly loaded content after e.g. clicking a pager
+  // link.
+  var offset = $(response.selector).offset();
+  // We can't guarantee that the scrollable object should be
+  // the body, as the view could be embedded in something
+  // more complex such as a modal popup. Recurse up the DOM
+  // and scroll the first element that has a non-zero top.
+  var scrollTarget = response.selector;
+  while ($(scrollTarget).scrollTop() == 0 && $(scrollTarget).parent()) {
+    scrollTarget = $(scrollTarget).parent()
+  }
+  // Only scroll upward
+  if (offset.top - 10 < $(scrollTarget).scrollTop()) {
+    $(scrollTarget).animate({scrollTop: (offset.top - 10)}, 500);
+  }
+};
+
 })(jQuery);
