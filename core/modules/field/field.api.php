f3835606c70530d903d596a9d288e12539bd5d37
Jen Lampton <jen@jeneration.com>
2013-09-05 22:40:22 -0700

    Issue #21: remove RDF module from core.

--- core/modules/field/field.api.php
+++ core/modules/field/field.api.php
@@ -1366,18 +1366,12 @@ function hook_field_attach_purge($entity_type, $entity, $field, $instance) {
  *   - language: The language code used for rendering.
  */
 function hook_field_attach_view_alter(&$output, $context) {
-  // Append RDF term mappings on displayed taxonomy links.
   foreach (element_children($output) as $field_name) {
     $element = &$output[$field_name];
     if ($element['#field_type'] == 'taxonomy_term_reference' && $element['#formatter'] == 'taxonomy_term_reference_link') {
       foreach ($element['#items'] as $delta => $item) {
         $term = $item['taxonomy_term'];
-        if (!empty($term->rdf_mapping['rdftype'])) {
-          $element[$delta]['#options']['attributes']['typeof'] = $term->rdf_mapping['rdftype'];
-        }
-        if (!empty($term->rdf_mapping['name']['predicates'])) {
-          $element[$delta]['#options']['attributes']['property'] = $term->rdf_mapping['name']['predicates'];
-        }
+        $element[$delta]['#options']['attributes']['title'] = t($term->name);
       }
     }
   }
