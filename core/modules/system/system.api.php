ecadd8266f5e1b56dbf35acde1806c191a76340b
Jen Lampton <jen@jeneration.com>
2013-09-05 22:48:21 -0700

    Issue #21: Remove RDF module from core.

--- core/modules/system/system.api.php
+++ core/modules/system/system.api.php
@@ -1539,11 +1539,7 @@ function hook_mail_alter(&$message) {
  *   The name of the module hook being implemented.
  */
 function hook_module_implements_alter(&$implementations, $hook) {
-  if ($hook == 'rdf_mapping') {
-    // Move my_module_rdf_mapping() to the end of the list. module_implements()
-    // iterates through $implementations with a foreach loop which PHP iterates
-    // in the order that the items were added, so to move an item to the end of
-    // the array, we remove it and then add it.
+  if ($hook == 'node_view') {
     $group = $implementations['my_module'];
     unset($implementations['my_module']);
     $implementations['my_module'] = $group;
