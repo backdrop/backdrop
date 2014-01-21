f95758a9adc53ed40d6dbf7366a1ceabdb05cdf9
Nathan Haug <nate@lullabot.com>
2014-01-20 21:27:35 -0800

    Issue #141: Introduce state system for environment-specific,  non-configuration settings.

--- core/cron.php
+++ core/cron.php
@@ -16,11 +16,11 @@ define('DRUPAL_ROOT', getcwd());
 include_once DRUPAL_ROOT . '/core/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
-if (!isset($_GET['cron_key']) || variable_get('cron_key', 'drupal') != $_GET['cron_key']) {
+if (!isset($_GET['cron_key']) || state_get('cron_key') != $_GET['cron_key']) {
   watchdog('cron', 'Cron could not run because an invalid key was used.', array(), WATCHDOG_NOTICE);
   drupal_access_denied();
 }
-elseif (variable_get('maintenance_mode', 0)) {
+elseif (state_get('maintenance_mode', 0)) {
   watchdog('cron', 'Cron could not run because the site is in maintenance mode.', array(), WATCHDOG_NOTICE);
   drupal_access_denied();
 }
