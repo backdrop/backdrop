4e4629b60a64c7619b3ca1e14a5ad70a2b189aaa
Jen Lampton <jen@jeneration.com>
2014-05-14 22:35:34 -0700

    Issue #2011578 by mortendk, tlattimore, Cottser, soulston: Markup for Stark's page.html.twig + maintenance-page.html.twig.

--- core/themes/bartik/template.php
+++ core/themes/bartik/template.php
@@ -67,6 +67,15 @@ function bartik_process_page(&$variables) {
     // Make sure the shortcut link is the first item in title_suffix.
     $variables['title_suffix']['add_or_remove_shortcut']['#weight'] = -100;
   }
+  // Alter the attributes on the main menu and secondary menu render arrays.
+  if (!empty($variables['main_menu'])) {
+    $variables['main_menu']['#attributes']['id'] = 'main-menu-links';
+    $variables['main_menu']['#attributes']['class'] = array('links', 'clearfix');
+  }
+  if (!empty($variables['secondary_menu'])) {
+    $variables['secondary_menu']['#attributes']['id'] = 'secondary-menu-links';
+    $variables['secondary_menu']['#attributes']['class'] = array('links', 'inline', 'clearfix');
+  }
 }
 
 /**
