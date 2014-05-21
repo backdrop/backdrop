9a6456bd5ad43cf80fed8f413813c02c3db6776a
Matthias Philippine <twinmat@gmail.com>
2014-05-21 13:34:50 -0700

    Issue #1684800 by nod_: JSHint drupal.js.

--- core/misc/drupal.js
+++ core/misc/drupal.js
@@ -1,5 +1,5 @@
 
-var Drupal = Drupal || { 'settings': {}, 'behaviors': {}, 'locale': {} };
+window.Drupal = Drupal || { 'settings': {}, 'behaviors': {}, 'locale': {} };
 
 // Allow other JavaScript libraries to use $.
 jQuery.noConflict();
