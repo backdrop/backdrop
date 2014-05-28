d31734a71d312eb16576aa5e51635a9226106bfe
Matthias Philippine <twinmat@gmail.com>
2014-05-28 11:18:03 -0700

    Issue #1574490 by frega, nod_: Clean-up: batch.js.

--- core/misc/batch.js
+++ core/misc/batch.js
@@ -1,34 +1,36 @@
-(function ($) {
+(function ($, Backdrop) {
 
 /**
  * Attaches the batch behavior to progress bars.
  */
 Backdrop.behaviors.batch = {
   attach: function (context, settings) {
-    $('#progress', context).once('batch', function () {
-      var holder = $(this);
-      // Remove HTML from no-js progress bar. The JS progress bar is created
-      // later on.
-      holder.empty();
-
-      // Success: redirect to the summary.
-      var updateCallback = function (progress, status, pb) {
-        if (progress == 100) {
-          pb.stopMonitoring();
-          window.location = settings.batch.uri + '&op=finished';
-        }
-      };
-
-      var errorCallback = function (pb) {
-        holder.prepend($('<p class="error"></p>').html(settings.batch.errorMessage));
-        $('#wait').hide();
-      };
-
-      var progress = new Backdrop.progressBar('updateprogress', updateCallback, 'POST', errorCallback);
-      progress.setProgress(-1, settings.batch.initMessage);
-      holder.append(progress.element);
-      progress.startMonitoring(settings.batch.uri + '&op=do', 10);
-    });
+    var batch = settings.batch;
+    var $progress = $('#progress').once('batch');
+    var progressBar;
+  
+     // Success: redirect to the summary.
+     function updateCallback(progress, status, pb) {
+       if (progress === '100') {
+         pb.stopMonitoring();
+         window.location = batch.uri + '&op=finished';
+       }
+     }
+  
+     function errorCallback(pb) {
+       $progress.prepend($('<p class="error"></p>').html(batch.errorMessage));
+       $('#wait').hide();
+     }
+  
+     if ($progress.length) {
+       progressBar = new Drupal.ProgressBar('updateprogress', updateCallback, 'POST', errorCallback);
+       progressBar.setProgress(-1, batch.initMessage);
+       progressBar.startMonitoring(batch.uri + '&op=do', 10);
+       // Remove HTML from no-js progress bar.
+       $progress.empty();
+       // Append the JS progressbar element.
+       $progress.append(progressBar.element);
+     }
   }
 };
 
