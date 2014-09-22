896d08ff4f6f677a1f7fde2ac527b36e44b1ef7f
tarekdj <tarekdj@gmail.com>
2014-09-22 10:29:00 +0100

    Issue #323: Rename "Admin menu" to "Admin bar".

--- core/modules/admin_bar/admin_bar.api.php
+++ core/modules/admin_bar/admin_bar.api.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * API documentation for Administration menu.
+ * API documentation for Administration bar.
  */
 
 /**
@@ -18,11 +18,11 @@
  *   combined with every other argument from the set (technically, the cartesian
  *   product of all arguments). The expansion values may be empty; that is, you
  *   do not need to insert logic to skip map items for which no values exist,
- *   since Administration menu will take care of that.
+ *   since Administration bar will take care of that.
  *
- * @see admin_menu.map.inc
+ * @see admin_bar.map.inc
  */
-function hook_admin_menu_map() {
+function hook_admin_bar_map() {
   // Expand content types below Structure > Content types.
   // The key denotes the dynamic path to expand to multiple menu items.
   $map['admin/structure/types/manage/%node_type'] = array(
@@ -39,32 +39,32 @@ function hook_admin_menu_map() {
 }
 
 /**
- * Add to the administration menu content before it is rendered.
+ * Add to the administration bar content before it is rendered.
  *
  * Only use this hook to add new data to the menu structure. Use
- * hook_admin_menu_output_alter() to *alter* existing data.
+ * hook_admin_bar_output_alter() to *alter* existing data.
  *
  * @param array $content
  *   A structured array suitable for backdrop_render(), potentially containing:
- *   - menu: The administrative menu of links below the path 'admin/*'.
+ *   - menu: The administrative bar of links below the path 'admin/*'.
  *   - icon: The icon menu.
  *   - account: The user account name and log out link.
  *   - users: The user counter.
  *   Additionally, these special properties:
  *   - #components: The actual components contained in $content are configurable
- *     and depend on the 'admin_menu.settings.components' configuration value.
+ *     and depend on the 'admin_bar.settings.components' configuration value.
  *     #components holds a copy of that for convenience.
  *   - #complete: A Boolean indicating whether the complete menu should be built,
  *     ignoring the current configuration in #components.
  *   Passed by reference.
  *
- * @see hook_admin_menu_output_alter()
- * @see admin_menu_links_menu()
- * @see admin_menu_links_icon()
- * @see admin_menu_links_user()
- * @see theme_admin_menu_links()
+ * @see hook_admin_bar_output_alter()
+ * @see admin_bar_links_menu()
+ * @see admin_bar_links_icon()
+ * @see admin_bar_links_user()
+ * @see theme_admin_bar_links()
  */
-function hook_admin_menu_output_build(&$content) {
+function hook_admin_bar_output_build(&$content) {
   // In case your implementation provides a configurable component, check
   // whether the component should be displayed:
   if (in_array('shortcut.links', $content['#components']) && !$content['#complete']) {
@@ -99,17 +99,17 @@ function hook_admin_menu_output_build(&$content) {
 }
 
 /**
- * Change the administration menu content before it is rendered.
+ * Change the administration bar content before it is rendered.
  *
  * Only use this hook to alter existing data in the menu structure. Use
- * hook_admin_menu_output_build() to *add* new data.
+ * hook_admin_bar_output_build() to *add* new data.
  *
  * @param array $content
  *   A structured array suitable for backdrop_render(). Passed by reference.
  *
- * @see hook_admin_menu_output_build()
+ * @see hook_admin_bar_output_build()
  */
-function hook_admin_menu_output_alter(&$content) {
+function hook_admin_bar_output_alter(&$content) {
 }
 
 /**
@@ -123,16 +123,16 @@ function hook_admin_menu_output_alter(&$content) {
  *   An associative array whose keys are jQuery selectors and whose values are
  *   strings containing the replacement content.
  */
-function hook_admin_menu_replacements($complete) {
+function hook_admin_bar_replacements($complete) {
   $items = array();
   // If the complete menu is output, then it is uncached and will contain the
   // current counts already.
   if (!$complete) {
     // Check whether the users count component is enabled.
-    $components = config_get('admin_menu.settings', 'components');
-    if (!empty($components['admin_menu.users']) && ($user_count = admin_menu_get_user_count())) {
+    $components = config_get('admin_bar.settings', 'components');
+    if (!empty($components['admin_bar.users']) && ($user_count = admin_bar_get_user_count())) {
       // Replace the counters in the cached menu output with current counts.
-      $items['.admin-menu-users a'] = $user_count;
+      $items['.admin-bar-users a'] = $user_count;
     }
   }
   return $items;
@@ -141,7 +141,7 @@ function hook_admin_menu_replacements($complete) {
 /**
  * Inform about additional module-specific caches that can be cleared.
  *
- * Administration menu uses this hook to gather information about available
+ * Administration bar uses this hook to gather information about available
  * caches that can be flushed individually. Each returned item forms a separate
  * menu link below the "Flush all caches" link in the icon menu.
  *
@@ -155,7 +155,7 @@ function hook_admin_menu_replacements($complete) {
  *     makes sense to users in both locations.
  *   - callback: The name of a function to invoke to flush the individual cache.
  */
-function hook_admin_menu_cache_info() {
+function hook_admin_bar_cache_info() {
   $caches['update'] = array(
     'title' => t('Update data'),
     'callback' => '_update_cache_clear',
