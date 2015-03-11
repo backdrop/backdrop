44d306abfd1a0f69530534f1bbf8586b08764e41
John Romine <jromine@uci.edu>
2015-03-10 17:54:34 -0700

    Issue #729: Add empty() test for $row_classes in views templates

--- core/modules/views/templates/views-view-grid.tpl.php
+++ core/modules/views/templates/views-view-grid.tpl.php
@@ -26,7 +26,7 @@
 
   <tbody>
     <?php foreach ($rows as $row_number => $columns): ?>
-      <tr <?php if ($row_classes[$row_number]) { print 'class="' . implode(' ', $row_classes[$row_number]) .'"';  } ?>>
+      <tr <?php if (!empty($row_classes[$row_number])) { print 'class="' . implode(' ', $row_classes[$row_number]) .'"';  } ?>>
         <?php foreach ($columns as $column_number => $item): ?>
           <td <?php if ($column_classes[$row_number][$column_number]) { print 'class="' . implode(' ', $column_classes[$row_number][$column_number]) .'"';  } ?>>
             <?php print $item; ?>
