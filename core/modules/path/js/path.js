640ab9d553c84b2db323e867ffd506aa8c8850ff
Nathan Haug <nate@lullabot.com>
2015-05-01 01:37:21 -0700

    Issue #87: Restructuring pathauto in core integration. Adding upgrade path.

--- core/modules/path/js/path.js
+++ core/modules/path/js/path.js
@@ -1,4 +1,3 @@
-
 /**
  * @file
  * Attaches behaviors for the Path module.
@@ -6,29 +5,11 @@
 
 (function ($) {
 
-/*
-Geoff commented this out when adding pathauto to path in favour of the below behaviour
-from pathauto.
-
-If this was the correct thing to do we should completely remove this.
-*/
-/*Backdrop.behaviors.pathFieldsetSummaries = {
-  attach: function (context) {
-    $('fieldset.path-form', context).backdropSetSummary(function (context) {
-      var path = $('.form-item-path-alias input').val();
-
-      return path ?
-        Backdrop.t('Alias: @alias', { '@alias': path }) :
-        Backdrop.t('No alias');
-    });
-  }
-};*/
-
 Backdrop.behaviors.pathFieldsetSummaries = {
   attach: function (context) {
     $('fieldset.path-form', context).backdropSetSummary(function (context) {
       var path = $('.form-item-path-alias input').val();
-      var automatic = $('.form-item-path-pathauto input').attr('checked');
+      var automatic = $('.form-item-path-auto input').attr('checked');
 
       if (automatic) {
           return Backdrop.t('Automatic alias');
--- core/modules/path/path.api.php
+++ core/modules/path/path.api.php
@@ -1,8 +1,36 @@
 <?php
-
 /**
  * @file
  * Hooks provided by the Path module.
+ *
+ * Path module provides automatic path aliasing by using tokens in path
+ * patterns. Thus the simplest integration is just to provide tokens using
+ * hook_token_info() and hook_tokens().
+ *
+ * If you wish to provide automatic path creation for custom paths provided by
+ * your module, there are a few steps involved.
+ *
+ * 1. hook_path_info()
+ *    Provide information required by Path for the settings form as well as
+ *    bulk generation. See the documentation for hook_path_info() for more
+ *    details.
+ *
+ * 2. path_generate_entity_alias()
+ *    At the appropriate time (usually when a new item is being created for
+ *    which a generated alias is desired), call path_generate_entity_alias()
+ *    with the appropriate parameters to generate the alias. Then save the
+ *    alias with path_save_automatic_alias(). See the user, taxonomy, and node
+ *    hook implementations for examples.
+ *
+ * 3. path_delete_all_by_source()
+ *    At the appropriate time (usually when an item is being deleted), call
+ *    path_delete_all_by_source() to remove any aliases that were created for the
+ *    content being removed. See the documentation for path_delete_all_by_source() for
+ *    more details.
+ *
+ * There are other integration points with Path module, namely alter hooks that
+ * allow you to change the data used by Path at various points in the
+ * process. See the below hook documentation for details.
  */
 
 /**
@@ -73,130 +101,47 @@ function hook_path_delete($path) {
  */
 
 /**
- * Added pathauto.api.php functions; moving pathauto contrib to
- * backdrop/core/path
- */
-/**
- * @file
- * Documentation for pathauto API.
- *
- * It may be helpful to review some examples of integration from
- * pathauto.pathauto.inc.
- *
- * Pathauto works by using tokens in path patterns.  Thus the simplest
- * integration is just to provide tokens.  Token support is provided by Drupal
- * core. To provide additional token from your module, implement the following
- * hooks:
- *
- * hook_tokens() - http://api.drupal.org/api/function/hook_tokens
- * hook_token_info() - http://api.drupal.org/api/function/hook_token_info
- *
- * If you wish to provide pathauto integration for custom paths provided by your
- * module, there are a few steps involved.
- *
- * 1. hook_pathauto()
- *    Provide information required by pathauto for the settings form as well as
- *    bulk generation. See the documentation for hook_pathauto() for more
- *    details.
- *
- * 2. pathauto_create_alias()
- *    At the appropriate time (usually when a new item is being created for
- *    which a generated alias is desired), call pathauto_create_alias() with the
- *    appropriate parameters to generate and create the alias. See the user,
- *    taxonomy, and node hook implementations in pathauto.module for examples.
- *    Also see the documentation for pathauto_create_alias().
- *
- * 3. pathauto_path_delete_all()
- *    At the appropriate time (usually when an item is being deleted), call
- *    pathauto_path_delete_all() to remove any aliases that were created for the
- *    content being removed.  See the documentation for
- *    pathauto_path_delete_all() for more details.
- *
- * 4. hook_path_alias_types()
- *    For modules that create new types of content that can be aliased with
- *    pathauto, a hook implementation is needed to allow the user to delete them
- *    all at once.  See the documentation for hook_path_alias_types() below for
- *    more information.
- *
- * There are other integration points with pathauto, namely alter hooks that
- * allow you to change the data used by pathauto at various points in the
- * process.  See the below hook documentation for details.
- */
-
-/**
- * Used primarily by the bulk delete form.  This hooks provides pathauto the
- * information needed to bulk delete aliases created by your module.  The keys
- * of the return array are used by pathauto as the system path prefix to delete
- * from the url_aliases table.  The corresponding value is simply used as the
- * label for each type of path on the bulk delete form.
- *
- * @return
- *   An array whose keys match the beginning of the source paths
- *   (e.g.: "node/", "user/", etc.) and whose values describe the type of page
- *   (e.g.: "Content", "Users"). Like all displayed strings, these descriptions
- *   should be localized with t(). Use % to match interior pieces of a path,
- *   like "user/%/track". This is a database wildcard (meaning "user/%/track"
- *   matches "user/1/track" as well as "user/1/view/track").
- */
-function hook_path_alias_types() {
-  $objects['user/'] = t('Users');
-  $objects['node/'] = t('Content');
-  return $objects;
-}
-
-/**
  * Provide information about the way your module's aliases will be built.
  *
  * The information you provide here is used to build the form
- * on search/path/patterns. File pathauto.pathauto.inc provides example
- * implementations for system modules.
+ * on search/path/patterns
  *
- * @see node_pathauto()
- *
- * @param $op
- *   At the moment this will always be 'settings'.
- *
- * @return object|null
- *   An object, or array of objects (if providing multiple groups of path
- *   patterns).  Each object should have the following members:
- *   - 'module': The module or entity type.
- *   - 'token_type': Which token type should be allowed in the patterns form.
- *   - 'group_header': Translated label for the settings group
- *   - 'pattern_description': The translated label for the default pattern (e.g.,
- *      t('Default path pattern (applies to all content types with blank
- *      patterns below)')
- *   - 'pattern_default': Default pattern  (e.g. 'content/[node:title]'
- *   - 'batch_update_callback': The name of function that should be ran for
- *      bulk update. @see node_path_bulk_update_batch_process for example
- *   - 'batch_file': The name of the file with the bulk update function.
- *   - 'pattern_items': Optional. An array of descriptions keyed by bundles.
+ * @return array
+ *   A 2-level array of automatic path settings. Each item should have a unique
+ *   key (often the name of the providing module). Each sub-array should contain
+ *   the following:
+ *   - token_type: Which token type should be allowed in the patterns form.
+ *   - group_header: Translated label for the settings group
+ *   - pattern_description: The translated label for the default pattern (e.g.,
+ *       t('Default path pattern (applies to all content types with blank
+ *       patterns below)')
+ *   - pattern_default: Default pattern  (e.g. 'content/[node:title]')
+ *   - batch_update_callback: The name of function that should be ran for
+ *       bulk update. See node_path_bulk_update_batch_process() for an example.
+ *   - batch_file: The name of the file with the bulk update function.
+ *   - pattern_items: Optional. An array of descriptions keyed by bundles.
  */
-function hook_path($op) {
-  switch ($op) {
-    case 'settings':
-      $settings = array();
-      $settings['module'] = 'file';
-      $settings['token_type'] = 'file';
-      $settings['group_header'] = t('File paths');
-      $settings['pattern_description'] = t('Default path pattern (applies to all file types with blank patterns below)');
-      $settings['pattern_default'] = 'files/[file:name]';
-      $settings['batch_update_callback'] = 'file_entity_path_bulk_update_batch_process';
-      $settings['batch_file'] = backdrop_get_path('module', 'file_entity') . '/file_entity.pathauto.inc';
+function hook_path_info() {
+  $info['file'] = array(
+    'module' => 'file',
+    'token_type' => 'file',
+    'group_header' => t('File paths'),
+    'pattern_description' => t('Default path pattern (applies to all file types with blank patterns below)'),
+    'pattern_default' => 'files/[file:name]',
+    'batch_update_callback' => 'file_entity_path_bulk_update_batch_process',
+    'batch_file' => backdrop_get_path('module', 'file') . '/file.path.inc',
+  );
 
-      foreach (file_type_get_enabled_types() as $file_type => $type) {
-        $settings['pattern_items'][$file_type] = t('Pattern for all @file_type paths.', array('@file_type' => $type->label));
-      }
-      return (object) $settings;
-
-    default:
-      break;
+  foreach (file_type_get_enabled_types() as $file_type => $type) {
+    $info['file']['pattern_items'][$file_type] = t('Pattern for all @file_type paths.', array('@file_type' => $type->label));
   }
+  return $info;
 }
 
 /**
  * Determine if a possible URL alias would conflict with any existing paths.
  *
- * Returning TRUE from this function will trigger pathauto_alias_uniquify() to
+ * Returning TRUE from this function will trigger path_alias_uniquify() to
  * generate a similar URL alias with a suffix to avoid conflicts.
  *
  * @param string $alias
@@ -210,7 +155,7 @@ function hook_path($op) {
  *   TRUE if $alias conflicts with an existing, reserved path, or FALSE/NULL if
  *   it does not match any reserved paths.
  *
- * @see pathauto_alias_uniquify()
+ * @see path_alias_uniquify()
  */
 function hook_path_is_alias_reserved($alias, $source, $langcode) {
   // Check our module's list of paths and return TRUE if $alias matches any of
