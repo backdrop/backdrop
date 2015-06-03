d1b07c545aa3700b69f0933ee01cfad53121e78a
Nathan Haug <nate@lullabot.com>
2015-06-02 22:49:08 -0600

    Issue #101: Expanding filter formats to include editor configuration.

--- core/modules/filter/js/filter.js
+++ core/modules/filter/js/filter.js
@@ -6,6 +6,11 @@
 (function ($) {
 
 /**
+ * Initialize an empty object where editors where place their attachment code.
+ */
+Backdrop.editors = {};
+
+/**
  * Displays the guidelines of the selected text format automatically.
  */
 Backdrop.behaviors.filterGuidelines = {
@@ -19,6 +24,66 @@ Backdrop.behaviors.filterGuidelines = {
           .siblings('.filter-guidelines-' + this.value).show();
       })
       .change();
+  }
+};
+
+/**
+ * Enables an editor (if any) when the matching format is selected.
+ */
+Backdrop.behaviors.filterEditors = {
+  attach: function (context, settings) {
+    // If there are no filter settings, there are no editors to enable.
+    if (!settings.filter) {
+      return;
+    }
+
+    var $context = $(context);
+    $context.find('.filter-list:input').once('filterEditors', function () {
+      var $this = $(this);
+      var activeEditor = $this.val();
+      var field = $this.closest('.text-format-wrapper').find('textarea').get(-1);
+
+      // Directly attach this editor, if the input format is enabled or there is
+      // only one input format at all.
+      if ($this.is(':input')) {
+        if (Backdrop.settings.filter.formats[activeEditor]) {
+          Backdrop.filterEditorAttach(field, Backdrop.settings.filter.formats[activeEditor]);
+        }
+      }
+      // Attach onChange handlers to input format selector elements.
+      if ($this.is('select')) {
+        $this.change(function() {
+          // Detach the current editor (if any) and attach a new editor.
+          if (Backdrop.settings.filter.formats[activeEditor]) {
+            Backdrop.filterEditorDetach(field, Backdrop.settings.filter.formats[activeEditor]);
+          }
+          activeEditor = $this.val();
+          if (Backdrop.settings.filter.formats[activeEditor]) {
+            Backdrop.filterEditorAttach(field, Backdrop.settings.filter.formats[activeEditor]);
+          }
+        });
+      }
+      // Detach any editor when the containing form is submitted.
+      $this.parents('form').submit(function (event) {
+        // Do not detach if the event was canceled.
+        if (event.isDefaultPrevented()) {
+          return;
+        }
+        Backdrop.filterEditorDetach(field, Backdrop.settings.filter.formats[activeEditor]);
+      });
+    });
+  }
+};
+
+Backdrop.filterEditorAttach = function(field, format) {
+  if (format.editor) {
+    Backdrop.editors[format.editor].attach(field, format);
+  }
+};
+
+Backdrop.filterEditorDetach = function(field, format) {
+  if (format.editor) {
+    Backdrop.editors[format.editor].detach(field, format);
   }
 };
 
