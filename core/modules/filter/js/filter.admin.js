04dc7951a24b838b80f3b486fd544cca3bdb24fa
docwilmot <docwilmot@gmail.com>
2015-08-02 10:26:25 -0500

    convert to draggable rows

--- core/modules/filter/js/filter.admin.js
+++ core/modules/filter/js/filter.admin.js
@@ -7,42 +7,75 @@
 
 Backdrop.behaviors.filterStatus = {
   attach: function (context, settings) {
-    $('#filters-status-wrapper input.form-checkbox', context).once('filter-status', function () {
-      var $checkbox = $(this);
-      // Retrieve the tabledrag row belonging to this filter.
-      var $row = $('#' + $checkbox.attr('id').replace(/-status$/, '-weight'), context).closest('tr');
-      // Retrieve the vertical tab belonging to this filter.
-      var tab = $('#' + $checkbox.attr('id').replace(/-status$/, '-settings'), context).data('verticalTab');
+    // tableDrag is required and we should be on the filters admin page.
+    // if (typeof Backdrop.tableDrag == 'undefined' || typeof Backdrop.tableDrag.filters == 'undefined') {
+    if (typeof Backdrop.tableDrag == 'undefined') {
+      return;
+    }
+    var table = $('table#filterorder');
+    var tableDrag = Backdrop.tableDrag.filterorder; // Get the filters tableDrag object.
 
-      // Bind click handler to this checkbox to conditionally show and hide the
-      // filter's tableDrag row and vertical tab pane.
-      $checkbox.bind('click.filterUpdate', function () {
-        if ($checkbox.is(':checked')) {
-          $row.show();
-          if (tab) {
-            tab.tabShow().updateSummary();
-          }
+    // Add a handler for when a row is swapped, update empty regions.
+    tableDrag.row.prototype.onSwap = function (swappedRow) {
+      checkEmptyRegions(table, this);
+    };
+
+    // A custom message for the filters page specifically.
+    Backdrop.theme.tableDragChangedWarning = function () {
+      return '<div class="messages warning">' + Backdrop.theme('tableDragChangedMarker') + ' ' + Backdrop.t('The changes to these filters will not be saved until the <em>Save filters</em> button is clicked.') + '</div>';
+    };
+
+    // Add a handler so when a row is dropped, update fields dropped into new regions.
+    tableDrag.onDrop = function () {
+      dragObject = this;
+      // Use "region-message" row instead of "region" row because
+      // "region-{region_name}-message" is less prone to regexp match errors.
+      dropIndex = dragObject.rowObject.element.rowIndex;
+      if(dragObject.oldRowElement.rowIndex != dropIndex) {
+        disableIndex = $('#filterorder tr.disable-label').index();
+        enabling = dropIndex < disableIndex;
+        dropRow = dragObject.rowObject.element;
+        $(dropRow).find('input:checkbox:first').prop('checked', enabling);
+      }
+    };
+
+    // Add the behavior to each region select list.
+    $('#filterorder input:checkbox', context).change(function (event) {
+        // Make our new row and select field.
+        var row = $(this).closest('tr');
+        tableDrag.rowObject = new tableDrag.row(row);
+
+        // Find the correct region and insert the row as the last in the region.
+        var labelRow = $('.disable-label');
+        if ($(this).prop('checked')) {
+          labelRow.before(row);
         }
         else {
-          $row.hide();
-          if (tab) {
-            tab.tabHide().updateSummary();
-          }
-        }
-        // Restripe table after toggling visibility of table row.
-        Backdrop.tableDrag['filter-order'].restripeTable();
-      });
+          labelRow.after(row);
+       }
+        
 
-      // Attach summary for configurable filters (only for screen-readers).
-      if (tab) {
-        tab.fieldset.backdropSetSummary(function (tabContext) {
-          return $checkbox.is(':checked') ? Backdrop.t('Enabled') : Backdrop.t('Disabled');
-        });
-      }
-
-      // Trigger our bound click handler to update elements to initial state.
-      $checkbox.triggerHandler('click.filterUpdate');
+        // Modify empty regions with added or removed fields.
+        checkEmptyRegions(table, row);
+        // Remove focus from selectbox.
+        $(this).get(0).blur();
     });
+
+    var checkEmptyRegions = function (table, rowObject) {
+        // This region has become empty.
+        if ($('.disable-label').next('tr').length == 0) {
+          $('table#filterorder tr:last').after('<tr class="region-empty"><td colspan=3>Empty</td></tr>');
+        }
+        else {
+          $('.disable-label').nextAll('.region-empty').remove();
+        }
+        if ($('.disable-label').prev('tr').length == 0) {
+          $('table#filterorder tr:first').before('<tr class="region-empty"><td colspan=3>Empty</td></tr>');
+        }
+        else {
+          $('.disable-label').prevAll('.region-empty').remove();
+        }
+    };
   }
 };
 
