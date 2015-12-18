af1d6f0483d0e2ae6e1639cd9fb422e7c99e0644
docwilmot <docwilmot@gmail.com>
2015-12-18 08:30:22 -0500

    Issue #698: Add Radix layouts to core

---
+++ core/layouts/boxton/layout--boxton.tpl.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * @file
+ * Template for the Boxton layout.
+ *
+ * Variables:
+ * - $title: The page title, for use in the actual HTML content.
+ * - $messages: Status and error messages. Should be displayed prominently.
+ * - $tabs: Tabs linking to any sub-pages beneath the current page
+ *   (e.g., the view and edit tabs when displaying a node.)
+ * - $action_links: Array of actions local to the page, such as 'Add menu' on
+ *   the menu administration interface.
+ * - $classes: Array of CSS classes to be added to the layout wrapper.
+ * - $attributes: Array of additional HTML attributes to be added to the layout
+ *     wrapper. Flatten using backdrop_attributes().
+ * - $content: An array of content, each item in the array is keyed to one
+ *   region of the layout. This layout supports the following sections:
+ *   - $content['header']
+ *   - $content['content']
+ *   - $content['footer']
+ */
+?>
+<div class="layout--boxton <?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  </div>
+
+  <?php if ($content['header']): ?>
+    <header class="l-header" role="banner" aria-label="<?php print t('Site header'); ?>">
+      <div class="container">
+        <?php print $content['header']; ?>
+      </div>
+    </header>
+  <?php endif; ?>
+
+  <?php if ($messages): ?>
+    <div class="l-messages container">
+      <?php print $messages; ?>
+    </div>
+  <?php endif; ?>
+
+  <div class="l-container">
+  <div class="l-container-inner container">
+    <div class="l-page-header">
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1 class="title" id="page-title">
+          <?php print $title; ?>
+        </h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+    </div>
+
+    <?php if ($tabs): ?>
+      <div class="tabs">
+        <?php print $tabs; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php print $action_links; ?>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12 l-contentcolumnmain">
+          <?php print $content['content']; ?>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+
+  <?php if ($content['footer']): ?>
+    <footer class="l-footer"  role="footer">
+      <div class="container">
+        <?php print $content['footer']; ?>
+      </div>
+    </footer>
+  <?php endif; ?>
+</div><!-- /.boxton -->
