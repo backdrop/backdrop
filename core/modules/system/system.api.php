2886b5aeadcc3b55b2e4e6ef1809dfd6de7086f2
Jen Lampton <jen@jeneration.com>
2016-01-08 00:03:35 -0800

    Issue #911352 by Liam Morland, Sylvain Lecoy, Joe Murray, Crell: Document that foreign keys may not be used by all drivers.

--- core/modules/system/system.api.php
+++ core/modules/system/system.api.php
@@ -2563,7 +2563,9 @@ function hook_requirements($phase) {
  * creation and alteration of the supported database engines.
  *
  * See the Schema API Handbook at http://drupal.org/node/146843 for details on
- * schema definition structures.
+ * schema definition structures. Note that foreign key definitions are for
+ * documentation purposes only; foreign keys are not created in the database,
+ * nor are they enforced by Backdrop.
  *
  * @return array
  *   A schema definition structure array. For each element of the
@@ -2615,6 +2617,8 @@ function hook_schema() {
       'nid_vid' => array('nid', 'vid'),
       'vid'     => array('vid')
     ),
+    // For documentation purposes only; foreign keys are not created in the
+    // database.
     'foreign keys' => array(
       'node_revision' => array(
         'table' => 'node_revision',
