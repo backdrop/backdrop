f717cb6cec523ba485a44305b25ec9e6c788330b
gifad <jfdhenin@free.fr>
2016-01-21 17:29:41 +0100

    Implement backdropuploadimage.

---
+++ core/misc/ckeditor/build-config copie lf.js
@@ -0,0 +1,127 @@
+﻿/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+/**
+ * This file was added automatically by CKEditor builder.
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) http://ckeditor.com/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) http://ckeditor.com/builder/3537ed289b077743d8ade4241005b97a
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) http://ckeditor.com/builder/download/3537ed289b077743d8ade4241005b97a
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
+ *    This file is not used by CKEditor, you may remove it.
+ *    Changing this file will not change your CKEditor configuration.
+ */
+
+var CKBUILDER_CONFIG = {
+	skin: 'moonocolor',
+	preset: 'full',
+	ignore: [
+		'.bender',
+		'bender.js',
+		'bender-err.log',
+		'bender-out.log',
+		'dev',
+		'.DS_Store',
+		'.editorconfig',
+		'.gitattributes',
+		'.gitignore',
+		'gruntfile.js',
+		'.idea',
+		'.jscsrc',
+		'.jshintignore',
+		'.jshintrc',
+		'less',
+		'.mailmap',
+		'node_modules',
+		'package.json',
+		'README.md',
+		'tests'
+	],
+	plugins : {
+		'a11yhelp' : 1,
+		'about' : 1,
+		'autogrow' : 1,
+		'basicstyles' : 1,
+		'bidi' : 1,
+		'blockquote' : 1,
+		'clipboard' : 1,
+		'codemirror' : 1,
+		'codesnippet' : 1,
+		'colorbutton' : 1,
+		'colordialog' : 1,
+		'contextmenu' : 1,
+		'dialogadvtab' : 1,
+		'div' : 1,
+		'divarea' : 1,
+		'elementspath' : 1,
+		'enterkey' : 1,
+		'entities' : 1,
+		'filebrowser' : 1,
+		'find' : 1,
+		'flash' : 1,
+		'floatingspace' : 1,
+		'font' : 1,
+		'format' : 1,
+		'forms' : 1,
+		'horizontalrule' : 1,
+		'htmlwriter' : 1,
+		'iframe' : 1,
+		'image' : 1,
+		'image2' : 1,
+		'indentblock' : 1,
+		'indentlist' : 1,
+		'justify' : 1,
+		'language' : 1,
+		'link' : 1,
+		'list' : 1,
+		'liststyle' : 1,
+		'magicline' : 1,
+		'maximize' : 1,
+		'newpage' : 1,
+		'pagebreak' : 1,
+		'pastefromword' : 1,
+		'pastetext' : 1,
+		'placeholder' : 1,
+		'preview' : 1,
+		'print' : 1,
+		'removeformat' : 1,
+		'resize' : 1,
+		'save' : 1,
+		'scayt' : 1,
+		'selectall' : 1,
+		'sharedspace' : 1,
+		'showblocks' : 1,
+		'showborders' : 1,
+		'smiley' : 1,
+		'sourcearea' : 1,
+		'sourcedialog' : 1,
+		'specialchar' : 1,
+		'stylescombo' : 1,
+		'tab' : 1,
+		'table' : 1,
+		'tableresize' : 1,
+		'tabletools' : 1,
+		'templates' : 1,
+		'toolbar' : 1,
+		'undo' : 1,
+		'uploadwidget' : 1,
+		'wsc' : 1,
+		'wysiwygarea' : 1
+	},
+	languages : {
+		'en' : 1,
+		'fr' : 1
+	}
+};
---
+++ core/modules/ckeditor/js/plugins/backdropuploadimage/plugin.js
@@ -0,0 +1,130 @@
+/**
+ * @file
+ * Allow direct upload by drag and dropping local images into CKEditor.
+ */
+
+'use strict';
+
+(function ($) {
+
+  CKEDITOR.plugins.add('backdropuploadimage', {
+    requires: 'uploadwidget',
+
+    onLoad: function () {
+      CKEDITOR.addCss(
+        '.cke_upload_uploading img{' +
+        'opacity: 0.3' +
+        '}'
+      );
+    },
+
+    init: function (editor) {
+      // Do not execute this paste listener if it will not be possible to upload file.
+      if (!CKEDITOR.plugins.clipboard.isFileApiSupported) {
+        return;
+      }
+
+      var entityType = (editor.element.$.attributes['data-dnd-context']) ? 'node' : 'file';
+
+      var fileTools = CKEDITOR.fileTools,
+      //uploadUrl = fileTools.getUploadUrl( editor.config, 'image' );
+        uploadUrl = Backdrop.settings.basePath + entityType + '/uploadimage';
+
+      // Handle images which are available in the dataTransfer.
+      fileTools.addUploadWidget(editor, 'uploadimage', {
+        supportedTypes: /image\/(jpeg|png|gif)/,
+
+        uploadUrl: uploadUrl,
+
+        fileToElement: function () {
+          var img = new CKEDITOR.dom.element('img');
+          img.setAttribute('src', loadingImage);
+          return img;
+        },
+
+        parts: {
+          img: 'img'
+        },
+
+        onUploading: function (upload) {
+          // Show the image during the upload.
+          this.parts.img.setAttribute('src', upload.data);
+        },
+
+        onUploaded: function (upload) {
+          var range = editor.createRange();
+          range.setStartBefore(this.wrapper);
+          range.setEndAfter(this.wrapper);
+          if (entityType == 'node') {
+            Backdrop.dnd.refreshLibraries();
+            var sid = upload.url;
+            var context = editor.element.$.attributes['data-dnd-context'].value;
+            Backdrop.dnd.fetchNode(context, sid, function() {
+              Backdrop.dnd.Nodes[sid].meta.entityType = entityType;
+              Backdrop.dnd.Nodes[sid].meta.type = 'image';
+              Backdrop.dnd.Nodes[sid].sas = '[node='+sid+':'+context+']';
+              Backdrop.dnd.insertAtom(sid);
+            });
+          }
+          else {
+          //var fid = upload.fid;
+            var str = upload.url;
+            var arr = str.split('@');
+            var fid = arr[0];
+            var url = arr[1];
+            var range = editor.getSelection().getRanges()[0];
+            var html = '<img alt="" src="'+url+'" data-file-id="'+fid+'">';
+            var element = CKEDITOR.htmlParser.fragment.fromHtml(html).children[0];
+            // Turn it into a proper DOM element, and insert it.
+            element = CKEDITOR.dom.element.createFromHtml(element.getOuterHtml());
+            // ∆ image, not image2 !
+            var widget = editor.widgets.initOn(element, "image", {'data-file-id': fid});
+            editor.editable().insertElementIntoRange(widget.wrapper, range);
+            widget.focus();
+          }
+        }
+      });
+
+      // Handle images which are not available in the dataTransfer.
+      // This means that we need to read them from the <img src="data:..."> elements.
+      editor.on('paste', function (evt) {
+        // For performance reason do not parse data if it does not contain img tag and data attribute.
+        if (!evt.data.dataValue.match(/<img[\s\S]+data:/i)) {
+          return;
+        }
+
+        var data = evt.data,
+        // Prevent XSS attacks.
+          tempDoc = document.implementation.createHTMLDocument(''),
+          temp = new CKEDITOR.dom.element(tempDoc.body),
+          imgs, img, i;
+
+        // Without this isReadOnly will not works properly.
+        temp.data('cke-editable', 1);
+        temp.appendHtml(data.dataValue);
+
+        imgs = temp.find('img');
+        for (i = 0; i < imgs.count(); i++) {
+          img = imgs.getItem(i);
+
+          // Image have to contain src=data:...
+          var isDataInSrc = img.getAttribute('src') && img.getAttribute('src').substring(0, 5) == 'data:',
+            isRealObject = img.data('cke-realelement') === null;
+
+          // We are not uploading images in non-editable blocs and fake objects (#13003).
+          if (isDataInSrc && isRealObject && !img.data('cke-upload-id') && !img.isReadOnly(1)) {
+            var loader = editor.uploadRepository.create(img.getAttribute('src'));
+            loader.upload(uploadUrl);
+            fileTools.markElement(img, 'uploadimage', loader.id);
+            fileTools.bindNotifications(editor, loader);
+          }
+        }
+
+        data.dataValue = temp.getHtml();
+      });
+    }
+  });
+
+  // Black rectangle which is shown before image is loaded.
+  var loadingImage = 'data:image/gif;base64,R0lGODlhDgAOAIAAAAAAAP///yH5BAAAAAAALAAAAAAOAA4AAAIMhI+py+0Po5y02qsKADs=';
+})(jQuery);
