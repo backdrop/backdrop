e389e5871f9a62e7cd1ba5be60c978c4bf1369f2
Gor Martsen <Gor.martsen@gmail.com>
2016-03-14 15:32:49 -0400

    She user and password in deploy log

--- core/scripts/init_deploy.sh
+++ core/scripts/init_deploy.sh
@@ -14,3 +14,4 @@ ln -s $GITLC_DEPLOY_DIR/.htaccess ./
 
 #install backdrop
 php $GITLC_DEPLOY_DIR/core/scripts/install.sh --root=$SITEPATH --db-url=mysql://$DATABASE_USER:$DATABASE_PASSWORD@localhost/$DATABASE_NAME --account-mail=$ACCOUNT_MAIL --account-name=$ACCOUNT_NAME --account-pass="$DATABASE_PASSWORD" --site-mail=$SITE_MAIL --site-name="$SITE_NAME"
+echo "USER: $ACCOUNT_NAME PASS: $DATABASE_PASSWORD"
