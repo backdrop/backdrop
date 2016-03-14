dd3bcb72e5a9b864397a48cd7f11f26f76d114bd
Gor Martsen <Gor.martsen@gmail.com>
2016-03-14 15:09:38 -0400

    Debug deploy!

--- core/scripts/init_deploy.sh
+++ core/scripts/init_deploy.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 echo "INIT"
 printenv
+
+#set site pacth
+SITEPATH="$HOME/domains/$DOMAIN"
+
+# Go to domain dir
+cd $SITEPATH
+
+#link backdrop files
+ln -s $GITLC_DEPLOY_DIR/* ./
+
+#install backdrop
+sh $GITLC_DEPLOY_DIR/core/scrips/install.sh --root=$SITEPATH --db-url=mysql://$DATABASE_USER:$DATABASE_PASSWORD@localhost/$DATABASE_NAME --account-mail=$ACCOUNT_MAIL --account-name=$ACCOUNT_NAME --site-mail=$SITE_MAIL --site-name=$SITE_NAME
