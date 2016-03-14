5284cf60d93333e70916152d58621cd16fc6ce43
Gor Martsen <Gor.martsen@gmail.com>
2016-03-14 15:28:40 -0400

    Fix site name and add password generation

--- core/scripts/init_deploy.sh
+++ core/scripts/init_deploy.sh
@@ -10,6 +10,7 @@ cd $SITEPATH
 
 #link backdrop files
 ln -s $GITLC_DEPLOY_DIR/* ./
+ln -s $GITLC_DEPLOY_DIR/.htaccess ./
 
 #install backdrop
-php $GITLC_DEPLOY_DIR/core/scripts/install.sh --root=$SITEPATH --db-url=mysql://$DATABASE_USER:$DATABASE_PASSWORD@localhost/$DATABASE_NAME --account-mail=$ACCOUNT_MAIL --account-name=$ACCOUNT_NAME --site-mail=$SITE_MAIL --site-name=$SITE_NAME
+php $GITLC_DEPLOY_DIR/core/scripts/install.sh --root=$SITEPATH --db-url=mysql://$DATABASE_USER:$DATABASE_PASSWORD@localhost/$DATABASE_NAME --account-mail=$ACCOUNT_MAIL --account-name=$ACCOUNT_NAME --account-pass="$DATABASE_PASSWORD" --site-mail=$SITE_MAIL --site-name="$SITE_NAME"
