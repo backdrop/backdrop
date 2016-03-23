4e4c4831299b2a1b76913802e629a0897ceebfb7
Gor Martsen <Gor.martsen@gmail.com>
2016-03-22 22:29:36 -0400

    Update script to use environment variables for user

--- core/scripts/init_deploy.sh
+++ core/scripts/init_deploy.sh
@@ -12,5 +12,5 @@ ln -s $GITLC_DEPLOY_DIR/* ./
 ln -s $GITLC_DEPLOY_DIR/.htaccess ./
 
 #install backdrop
-php $GITLC_DEPLOY_DIR/core/scripts/install.sh --root=$SITEPATH --db-url=mysql://$DATABASE_USER:$DATABASE_PASSWORD@localhost/$DATABASE_NAME --account-mail=$ACCOUNT_MAIL --account-name=$ACCOUNT_NAME --account-pass="$DATABASE_PASSWORD" --site-mail=$SITE_MAIL --site-name="$SITE_NAME"
-echo "USER: $ACCOUNT_NAME PASS: $DATABASE_PASSWORD"
+php $GITLC_DEPLOY_DIR/core/scripts/install.sh --root=$SITEPATH --db-url=mysql://$DATABASE_USER:$DATABASE_PASSWORD@localhost/$DATABASE_NAME --account-mail=$ACCOUNT_MAIL --account-name=$SITE_USER --account-pass="$SITE_PASSWORD" --site-mail=$SITE_MAIL --site-name="$SITE_NAME"
+echo "USER: $SITE_USER PASS: $SITE_PASSWORD"
