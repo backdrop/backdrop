b50658cf923f158855b835f477334b7ae7b94505
Alex Finnarn <alexfinnarn@gmail.com>
2016-03-25 13:39:06 -0600

    Issue #1752 - Add check for local settings file

---
+++ example.settings.local.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * Local development override configuration feature.
+ *
+ * To activate this feature, copy and rename it such that its filename is
+ * 'settings.local.php'.
+ */
+// @todo Add examples of settings commonly changed during local development
