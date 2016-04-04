b5755e98a172b229e3ebb6134e508acbff81db96
docwilmot <docwilmot@gmail.com>
2016-04-04 08:32:38 -0500

    Add scroll link to project list TH on mobile

--- core/modules/installer/js/detail-dialog.js
+++ core/modules/installer/js/detail-dialog.js
@@ -6,6 +6,7 @@
  */
 (function ($) {
   $(document).ready(function() {
+    // Open all links in dialogs in a new window.
     $(window).on( "dialog:aftercreate", function( event, ui ) {
       $('a').each(function() {
          var a = new RegExp('/' + window.location.host + '/');
@@ -17,6 +18,46 @@
              });
          }
       });
+    });
+
+    // On mobile add an indicator of number of projects installed and scroll to
+    // installation queue on click.
+    var windowsize = $(window).width();
+    var header = $("#installer-browser-main th").html();
+    var items = $('.installer-browser-install-queue-item').length;
+    checkQueue();
+
+    function checkQueue() {
+      items = $('.installer-browser-install-queue-item').length;
+      if (windowsize < 768) {
+        updateTH();
+      }
+    }
+
+    function updateTH() {
+      $("#installer-browser-main th").html(header+': <span class="projects-selected">'+items+' selected. <a id="status-count-link" href="ff">review and install</a></span>');
+    }
+
+    $(document).ajaxComplete(function() {
+      checkQueue();
+    });
+
+    $(window).resize(function() {
+      windowsize = $(window).width();
+      if (windowsize < 768) {
+        updateTH();
+      }
+      else {
+        $("#installer-browser-main th").html(header);
+      }
+    });
+    
+    $('body').on('click', '#status-count-link', function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      $('html, body').animate({
+          scrollTop: $("#installer-browser-sidebar-right").offset().top
+      }, 400);
     });
   });
 })(jQuery);
