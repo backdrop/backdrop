a3d798edacf656197c1dd18b94944a68e2243e23
Jen Lampton <jenlampton@users.noreply.github.com>
2016-05-13 15:56:14 -0500

    Issue #1300 by docwilmot: Add ability to add classes to layout regions.

--- core/modules/layout/layout.api.php
+++ core/modules/layout/layout.api.php
@@ -127,25 +127,29 @@ function hook_layout_context_info() {
  *   include:
  *   - title: The translated title of the style.
  *   - description: The translated description of the style.
- *   - region theme: Optional. If this style needs to modify the display of an
- *     entire region, specify the theme function/template key that would be
- *     passed into the theme() function.
  *   - block theme: Optional. If this style modifies the display of blocks,
- *     specify the theme function/template key that would be passed into the
+ *     specify the theme function or template key that would be passed into the
  *     theme function.
  *   - class: Optional. The name of a class which contains any advanced methods
  *     to configure and save settings for this display style. If not specified,
  *     the default class of LayoutStyle will be used. Each class must also be
  *     registered in hook_autoload_info().
- *   - file: Optional. If using theme or preprocess functions, specify the path
- *     to the containing file. This file path is NOT the path to the class.
- *     Class paths and loading is done through hook_autoload_info().
- *   - path: The relative path from this module to the directory containing any
- *     templates or include files needed by this style.
+ *   - file: Optional. The name of the file the implementation resides in. This
+ *     file path is NOT the path to the class. Class paths and loading is done
+ *     through hook_autoload_info().
+ *   - path: Optional. Override the path to the file to be used. Ordinarily
+ *     theme functions are located in a file in the module path (for example:
+ *     mymodule/mymodule.theme.inc) and template files are located in a
+ *     subdirectory named templates (for example: mymodule/templates/), but if
+ *     your file will not be in the default location, include it here. This
+ *     path should be relative to the Backdrop root directory.
+ *   - template: If specified, this theme implementation is a template, and
+ *     this is the template file name without an extension. Do not include the
+ *     extension .tpl.php; it will be added automatically. If 'path' is
+ *     specified, then the template should be located in this path.
  *   - hook theme: Optional. If specified, additional information to be merged
  *     into hook_theme() on behalf of this style. This may be necessary if the
- *     values provided for "region theme" and "block theme" have not already
- *     been registered.
+ *     values provided for "block theme" have not already been registered.
  *
  * @see LayoutStyle
  * @see hook_autoload_info()
@@ -155,16 +159,15 @@ function hook_layout_style_info() {
   $info['custom_style'] = array(
     'title' => t('A new style'),
     'description' => t('An advanced style with settings.'),
-    // The theme key indicating what theme function/template will be used.
-    'region theme' => 'mymodule_layout_region',
     // The theme key for rendering an individual block.
     'block theme' => 'mymodule_block',
     // Provide a class name if this style has settings. The class should extend
     // the LayoutStyle class.
     'class' => 'MyModuleLayoutStyle',
-    // If nesting templates or a theme include file in a directory, the relative
-    // path can be specified.
-    'path' => 'templates',
+    // Override the path to the file to be used.
+    'path' => 'templates/subdir',
+    // Name of template file (with or without path).
+    'template' => 'templates/my-filename',
   );
   return $info;
 }
