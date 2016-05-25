457ddf5d22081e9ffec0c73e7c1d25cc6d22e86a
Gor Martsen <Gor.martsen@gmail.com>
2016-05-25 15:05:43 -0400

    Hide curl output

--- core/misc/zen-ci/run_tests.sh
+++ core/misc/zen-ci/run_tests.sh
@@ -7,16 +7,16 @@ SITEPATH="$HOME/www"
 echo "Full site path: $SITEPATH"
 cd $SITEPATH
 
-curl -X PUT -H "Content-Type: application/json" -H "Token: $GITLC_API_TOKEN" $GITLC_STATUS_URL --data '{"state": "pending", "message": "Processing Tests"}'
+curl -X PUT -H "Content-Type: application/json" -H "Token: $GITLC_API_TOKEN" $GITLC_STATUS_URL --data '{"state": "pending", "message": "Processing Tests"}' > /dev/null
 
 php core/scripts/run-tests.sh --url http://localhost --verbose --cache --force --all --concurrency 10 --color --verbose --summary /tmp/summary
 
 if [ $? -eq 0 ]; then 
   MESSAGE=`cat /tmp/summary| sed -n 1p| tr '\n' ' '`
-  curl -X PUT -H "Content-Type: application/json" -H "Token: $GITLC_API_TOKEN" $GITLC_STATUS_URL --data "{\"state\": \"success\", \"message\": \"$MESSAGE\"}"
+  curl -X PUT -H "Content-Type: application/json" -H "Token: $GITLC_API_TOKEN" $GITLC_STATUS_URL --data "{\"state\": \"success\", \"message\": \"$MESSAGE\"}" > /dev/null
 else
   MESSAGE=`cat /tmp/summary| sed -n 1p| tr '\n' ' '`
   SUMMARY=`cat /tmp/summary| sed ':a;N;$!ba;s/\n/\\n/g' | sed ':a;N;$!ba;s/"/\"/g'`
-  curl -X PUT -H "Content-Type: application/json" -H "Token: $GITLC_API_TOKEN" $GITLC_STATUS_URL --data "{\"state\": \"error\", \"message\": \"$MESSAGE\", \"summary\": \"$SUMMARY\" }"
+  curl -X PUT -H "Content-Type: application/json" -H "Token: $GITLC_API_TOKEN" $GITLC_STATUS_URL --data "{\"state\": \"error\", \"message\": \"$MESSAGE\", \"summary\": \"$SUMMARY\" }" > /dev/null
   exit 1
 fi
