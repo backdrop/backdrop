d47454cb301b5ba7301a9f8266861279a326a7d8
Gor Martsen <Gor.martsen@gmail.com>
2016-05-25 15:41:21 -0400

    Replace with php script

---
+++ core/misc/zen-ci/run_tests.php
@@ -0,0 +1,71 @@
+<?php
+  
+$home = getenv('HOME');
+
+
+$sitepath = $home . '/www';
+
+$data = array(
+  'state' => 'pending',
+  'message' => 'Processing Tests',
+);
+
+putRequest($data);
+
+exec('php core/scripts/run-tests.sh --url http://localhost --verbose --cache --force --all --concurrency 10 --color --verbose --summary /tmp/summary', $output, $status);
+
+$content = file_get_contents('/tmp/summary');
+
+if($status) {
+  $content = explode("\n", $content);
+  
+  $message = $content[0];
+  unset($content[0]);
+  $summary = implode("\n", $content);
+  //Test failed
+  $data = array(
+    'state' => 'error',
+    'message' => $message,
+    'summary' => $summary,
+  );
+  putRequest($data);
+  exit(1);
+}
+else {
+  //success
+  $data = array(
+    'state' => 'success',
+    'message' => $content,
+  );
+  exit(0);
+}
+
+
+
+function putRequest($data) {
+
+  $token = getenv('GITLC_API_TOKEN');
+  $status_url = getenv('GITLC_STATUS_URL');
+  
+  $data = json_encode($data);
+  
+  $ch = curl_init();
+
+  curl_setopt($ch, CURLOPT_URL, $status_url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_POST, true);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT"); // note the PUT here
+  
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+  curl_setopt($ch, CURLOPT_HEADER, true);
+  
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+      'Content-Type: application/json',
+      'Token: ' . $token,                                                                                
+      'Content-Length: ' . strlen($data)                                                                       
+  )); 
+  $output = curl_exec($ch);
+  echo($output) . PHP_EOL;
+  
+  curl_close($ch);
+}
