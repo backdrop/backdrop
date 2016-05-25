8465ccceb26f2d92d9722c2f1bb7812575aa5fd5
Gor Martsen <Gor.martsen@gmail.com>
2016-05-25 12:34:02 -0400

    Use summary file to put status

--- core/misc/zen-ci/run_tests.sh
+++ core/misc/zen-ci/run_tests.sh
@@ -7,4 +7,16 @@ SITEPATH="$HOME/www"
 echo "Full site path: $SITEPATH"
 cd $SITEPATH
 
-php core/scripts/run-tests.sh --url http://localhost --verbose --cache --force --all --concurrency 10 --color --verbose --zenci
+curl -X PUT -H "Content-Type: application/json" -H "Token: $GITLC_API_TOKEN" $GITLC_STATUS_URL -v --data '{"state": "pending", "message": "Processing Tests"}'
+
+php core/scripts/run-tests.sh --url http://localhost --verbose --cache --force --all --concurrency 10 --color --verbose --summary /tmp/summary
+
+if [$? -eq 0 ]; then 
+  MESSAGE = `cat /tmp/summary`
+  curl -X PUT -H "Content-Type: application/json" -H "Token: $GITLC_API_TOKEN" $GITLC_STATUS_URL -v --data '{"state": "success", "message": "'$MESSAGE'"}'
+else
+  MESSAGE = `cat /tmp/summary| sed -n 1p`
+  SUMMARY = `cat /tmp/summary`
+  curl -X PUT -H "Content-Type: application/json" -H "Token: $GITLC_API_TOKEN" $GITLC_STATUS_URL -v --data '{"state": "error", "message": "'$MESSAGE'", "summary": "'$SUMMARY'" }'
+  exit(1)
+fi
