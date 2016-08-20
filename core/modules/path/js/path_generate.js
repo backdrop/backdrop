68c532ea9e8ef53c0c0b1e009e974004ea21a953
docwilmot <docwilmot@gmail.com>
2016-08-20 14:17:21 -0500

    Add js toggles to checkboxes

--- core/modules/path/js/path_generate.js
+++ core/modules/path/js/path_generate.js
@@ -5,51 +5,47 @@
 (function ($) {
 Backdrop.behaviors.path_generate = {
   attach: function (context) {
-	  $("input.path-reset-type, input.path-reset-base").prop('disabled', true);
+	  $("input.path-reset").prop('disabled', true);
 
+    // When you select an entity checkbox (like "content"), select the 
+    // checkboxes for all the subtypes
 	  $("input.path-base").change(function () {
-    console.log(getNameParts($(this).attr('name')));
-      nameParts = getNameParts($(this).attr('name'));
-      $('table#permissions').find("input.path-"+nameParts[0]).prop('checked', this.checked);
-      updateReset($(this).prop('checked'), nameParts);
+      var type = $(this).attr('data-path-type');
+      $("input.path-type[data-path-type='"+type+"']").prop('checked', this.checked);
+      updateReset($(this).prop('checked'), type);
 	  });
 
-		// If you uncheck a child checkbox, uncheck the base checkbox too.
+		// If you uncheck a subtype checkbox, uncheck the entity checkbox.
 	  $("input.path-type").change(function () {
+      var type = $(this).attr('data-path-type');
 			if($(this).is(":checked") == false) {
-        nameParts = getNameParts($(this).attr('name'));
-        name = 'update['+nameParts[0]+'][base]';
-        $('table#permissions').find("input[name='"+name+"']").prop('checked', false);
+        $("input.path-base[data-path-type='"+type+"']").prop('checked', false);
   		}
-      updateReset($(this).prop('checked'), nameParts);
+      updateReset($(this).prop('checked'), type, $(this).attr('data-path-name'));
     });
 
+    // When you select an entity reset checkbox (like "content"), select the 
+    // reset checkboxes for all the subtypes.
 	  $("input.path-reset-base").change(function () {
-      console.log("base");
-      nameParts = getNameParts($(this).attr('name'));
-		  $('table#permissions').find("input.path-reset-"+nameParts[0]).prop('checked', this.checked);
+      var type = $(this).attr('data-path-type');
+		  $("input.path-reset[data-path-type='"+type+"']").prop('checked', this.checked);
 	  });
 
     /**
      * Disables the reset_alias checkboxes if path alias checkbox is unchecked.
      */
-    function getNameParts(name) {
-      tag1 = name.split('][');
-      tag2 = tag1[0].split('[');
-      tag3 = tag1[1].replace(']', '');
-      return [tag2[1], tag3];
-    }
-
-    /**
-     * Disables the reset_alias checkboxes if path alias checkbox is unchecked.
-     */
-    function updateReset(checked, nameParts) {
-      resetName = 'reset_alias'+'['+nameParts[0]+']['+nameParts[1]+']';
-      if(checked) {
-   		  $('table#permissions').find("input[name='"+resetName+"']").prop('disabled', false);
+    function updateReset(checked, type, name) {
+      if(name) {
+        $reset = $("input.path-reset[data-path-name='"+name+"']"); 
       }
       else {
-   		  $('table#permissions').find("input[name='"+resetName+"']").prop('checked', false).prop('disabled', true);
+        $reset = $("input.path-reset[data-path-type='"+type+"']"); 
+      }
+      if(checked) {
+   		  $($reset).prop('disabled', false);
+      }
+      else {
+   		  $($reset).prop('checked', false).prop('disabled', true);
       }
     }
 
