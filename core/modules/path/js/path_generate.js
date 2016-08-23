20962d0904b509c6303f3c4700fd831eb83f021f
docwilmot <docwilmot@users.noreply.github.com>
2016-08-23 10:31:48 -0500

    Fix indents and stray tabs in js file

--- core/modules/path/js/path_generate.js
+++ core/modules/path/js/path_generate.js
@@ -2,11 +2,13 @@
  * @file
  * Manages checkboxes for Path generation page.
  */
+
 (function ($) {
 Backdrop.behaviors.path_generate = {
   attach: function (context) {
-	  $("input.path-reset").prop('disabled', true);
+    $("input.path-reset").prop('disabled', true);
 
+    // Hide the Update existing checkboxes if deleting aliases
     $("select[name='operations[operation]']").change(function(value){
       $delete = ($(this).val()=='delete');
       $("table#path-bulk-alias").toggleClass('delete', $delete);
@@ -14,19 +16,19 @@ Backdrop.behaviors.path_generate = {
 
     // When you select an entity checkbox (like "content"), select the 
     // checkboxes for all the subtypes
-	  $("input.path-base").change(function () {
+    $("input.path-base").change(function () {
       var type = $(this).attr('data-path-type');
       $("input.path-type[data-path-type='"+type+"']").prop('checked', this.checked);
       updateReset($(this).prop('checked'), type);
-	  });
+    });
 
-		// If you uncheck a subtype checkbox, uncheck the entity checkbox.
-	  $("input.path-type").change(function () {
+    // If you uncheck a subtype checkbox, uncheck the entity checkbox.
+    $("input.path-type").change(function () {
       var type = $(this).attr('data-path-type');
       var $base = $("input.path-base[data-path-type='"+type+"']");
-			if($(this).is(":checked") == false) {
+      if($(this).is(":checked") == false) {
         $base.prop('checked', false);
-  		}
+      }
       else {
         unchecked = $("input.path-type[data-path-type='"+type+"']:checkbox:not(:checked)");
         if(unchecked.length < 1) {
@@ -39,12 +41,12 @@ Backdrop.behaviors.path_generate = {
 
     // When you select an entity reset checkbox (like "content"), select the 
     // reset checkboxes for all the subtypes.
-	  $("input.path-reset-base").change(function () {
+    $("input.path-reset-base").change(function () {
       var type = $(this).attr('data-path-type');
-		  $("input.path-reset[data-path-type='"+type+"']").prop('checked', this.checked);
-	  });
+      $("input.path-reset[data-path-type='"+type+"']").prop('checked', this.checked);
+    });
 
-    // Find all <th> with class select-all, and insert the check all checkbox.
+    // Add a check all checkbox in the table head row.
     var strings = { 'selectAll': Backdrop.t('Select all rows in this table'), 'selectNone': Backdrop.t('Deselect all rows in this table') };
     $('th.path-th-alias').prepend($('<input type="checkbox" class="form-checkbox" />').attr('title', strings.selectAll)).click(function (event) {
       if ($(event.target).is('input[type="checkbox"]')) {
@@ -55,7 +57,7 @@ Backdrop.behaviors.path_generate = {
       }
     });
 
-  /**
+    /**
      * Disables the reset_alias checkboxes if path alias checkbox is unchecked.
      */
     function updateReset(checked, type, name) {
@@ -66,15 +68,12 @@ Backdrop.behaviors.path_generate = {
         $reset = $("input.path-reset[data-path-type='"+type+"']"); 
       }
       if(checked) {
-   		  $($reset).prop('disabled', false);
+        $($reset).prop('disabled', false);
       }
       else {
-   		  $($reset).prop('checked', false).prop('disabled', true);
+        $($reset).prop('checked', false).prop('disabled', true);
       }
     }
-
-
   }
 };
-
 })(jQuery);
