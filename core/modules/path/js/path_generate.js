1baeea1037b35d70bc9f0e855594658561171e7f
docwilmot <docwilmot@gmail.com>
2016-09-05 23:38:38 -0500

    Fix checkbox behaviors

--- core/modules/path/js/path_generate.js
+++ core/modules/path/js/path_generate.js
@@ -6,7 +6,6 @@
 (function ($) {
 Backdrop.behaviors.path_generate = {
   attach: function (context) {
-    $("input.path-reset").prop('disabled', true);
 
     // Hide the Update existing checkboxes if deleting aliases
     $("select[name='operations[operation]']").change(function(value){
@@ -19,24 +18,23 @@ Backdrop.behaviors.path_generate = {
     $("input.path-base").change(function () {
       var type = $(this).attr('data-path-type');
       $("input.path-type[data-path-type='"+type+"']").prop('checked', this.checked);
-      updateReset($(this).prop('checked'), type);
     });
 
-    // If you uncheck a subtype checkbox, uncheck the entity checkbox.
     $("input.path-type").change(function () {
       var type = $(this).attr('data-path-type');
       var $base = $("input.path-base[data-path-type='"+type+"']");
+      // If you uncheck a subtype checkbox, uncheck the entity checkbox.
       if($(this).is(":checked") == false) {
         $base.prop('checked', false);
+        $("input.select-all-alias").prop('checked', false);
       }
+      // If all subtype checkboxes checked, check the base checkbox.
       else {
         unchecked = $("input.path-type[data-path-type='"+type+"']:checkbox:not(:checked)");
         if(unchecked.length < 1) {
           $base.prop('checked', true);
         }
       }
-      updateReset($(this).prop('checked'), type, $(this).attr('data-path-name'));
-      updateReset($base.prop('checked'), type, $base.attr('data-path-name'));
     });
 
     // When you select an entity reset checkbox (like "content"), select the 
@@ -46,34 +44,47 @@ Backdrop.behaviors.path_generate = {
       $("input.path-reset[data-path-type='"+type+"']").prop('checked', this.checked);
     });
 
-    // Add a check all checkbox in the table head row.
+    $("input.path-reset").change(function () {
+      var type = $(this).attr('data-path-type');
+      var $base = $("input.path-reset-base[data-path-type='"+type+"']");
+      // If you uncheck a subtype checkbox, uncheck the entity checkbox.
+      if($(this).is(":checked") == false) {
+        $base.prop('checked', false);
+      }
+      // If all subtype checkboxes checked, check the base checkbox.
+      else {
+        unchecked = $("input.path-reset[data-path-type='"+type+"']:checkbox:not(:checked)");
+        if(unchecked.length < 2) {
+          $base.prop('checked', true);
+        }
+      }
+    });
+
+    // Add check all checkboxes in the table head row.
     var strings = { 'selectAll': Backdrop.t('Select all rows in this table'), 'selectNone': Backdrop.t('Deselect all rows in this table') };
-    $('th.path-th-alias').prepend($('<input type="checkbox" class="form-checkbox" />').attr('title', strings.selectAll)).click(function (event) {
+    $('th.path-th-alias').prepend($('<input type="checkbox" class="select-all-alias form-checkbox" />').attr('title', strings.selectAll)).click(function (event) {
       if ($(event.target).is('input[type="checkbox"]')) {
         $("input.path-type, input.path-base").each(function () {
           this.checked = event.target.checked;
-          updateReset($(this).prop('checked'), $(this).attr('data-path-type'), $(this).attr('data-path-name'));
+        });
+      }
+    });
+    $('th.path-th-delete').prepend($('<input type="checkbox" class="select-all-delete form-checkbox" />').attr('title', strings.selectAll)).click(function (event) {
+      if ($(event.target).is('input[type="checkbox"]')) {
+        $("input.path-reset").each(function () {
+          this.checked = event.target.checked;
         });
       }
     });
 
-    /**
-     * Disables the reset_alias checkboxes if path alias checkbox is unchecked.
-     */
-    function updateReset(checked, type, name) {
-      if(name) {
-        $reset = $("input.path-reset[data-path-name='"+name+"']"); 
-      }
-      else {
-        $reset = $("input.path-reset[data-path-type='"+type+"']"); 
-      }
-      if(checked) {
-        $($reset).prop('disabled', false);
-      }
-      else {
-        $($reset).prop('checked', false).prop('disabled', true);
-      }
-    }
+    // Uncheck the "select all" checkboxes if not all checkboxes schecked.
+    $('input[type="checkbox"]').change(function () {
+      uncheckedAll = ($("input.path-type:checkbox:not(:checked)").length)+($("input.path-base:checkbox:not(:checked)").length);
+      $("input.select-all-alias").prop('checked', uncheckedAll<1);
+      uncheckedAllDelete = $("input.path-reset:checkbox:not(:checked)").length;
+      $("input.select-all-delete").prop('checked', uncheckedAllDelete<1);
+    });
+
   }
 };
 })(jQuery);
