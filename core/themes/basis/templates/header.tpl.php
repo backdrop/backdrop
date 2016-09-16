97e66263acd21f5eff6a6c856f8e45d7d58ba0a8
Jen Lampton <jen@jeneration.com>
2016-09-15 18:15:43 -0700

    Issue #2198: Remove the utility clases from Basis.

--- core/themes/basis/templates/header.tpl.php
+++ core/themes/basis/templates/header.tpl.php
@@ -8,12 +8,13 @@
  * - $base_path: The base URL path of the Backdrop installation. At the very
  *   least, this will always default to /.
  * - $directory: The directory the template is located in, e.g. modules/system
- *   or themes/bartik.
+ *   or themes/basis/templates.
  * - $is_front: TRUE if the current page is the front page.
  * - $logged_in: TRUE if the user is registered and signed in.
- * - $logo: The path to the logo image, as defined in theme configuration.
- * - $front_page: The URL of the front page. Use this instead of $base_path, when
- *   linking to the front page. This includes the language domain or prefix.
+ * - $front_page: The URL of the front page. Use this instead of $base_path when
+ *   linking to the front page since it includes the language domain or prefix.
+ *
+ * - $logo: The path to the logo image, as defined in site configuration.
  * - $site_name: The name of the site, empty when display has been disabled.
  * - $site_slogan: The site slogan, empty when display has been disabled.
  * - $menu: The menu for the header (if any), as an HTML string.
@@ -30,9 +31,9 @@
               <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header-logo" <?php print backdrop_attributes($logo_attributes); ?> />
             </div>
           <?php endif; ?>
-          <strong class="semantic">
-            <?php print $site_name; ?>
-          </strong>
+          <?php if ($site_name): ?>
+            <strong><?php print $site_name; ?></strong>
+          <?php endif; ?>
         </a>
       </div>
       <?php if ($site_slogan): ?>
