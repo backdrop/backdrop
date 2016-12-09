abeccd65a9c8e3b198fdd7dbfd3963e164d26b32
Nate Haug <nate@quicksketch.org>
2016-12-08 17:54:10 -0800

    Issue #2190: Add template files for layouts and the hero block to achieve a full-bleed hero.

--- core/modules/layout/templates/block.tpl.php
+++ core/modules/layout/templates/block.tpl.php
@@ -17,7 +17,7 @@
 <?php print render($title_prefix); ?>
 <?php if ($title): ?>
   <h2 class="block-title"><?php print $title; ?></h2>
-<?php endif;?>
+<?php endif; ?>
 <?php print render($title_suffix); ?>
 
   <div class="block-content">
