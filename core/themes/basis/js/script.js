16c8c82155c9c31f33744395c9943bfc56ee8a27
Herb vd Dool <herb@freeform.ca>
2016-12-26 16:14:47 -0500

    Revert "Provide option for menu toggle button Issue #2369"
    This reverts commit 7f154561e94f213aa28ce092828f7e70c07a3da6.

--- core/themes/basis/js/script.js
+++ core/themes/basis/js/script.js
@@ -14,31 +14,6 @@ Backdrop.behaviors.toggles = {
   }
 };
 
-Backdrop.behaviors.menuToggle = {
-  attach: function(context, settings) {
-    if (Backdrop.settings.basis.menuToggle == true) {
-      var $mainMenuState = $('#main-menu-state');
-      if ($mainMenuState.length) {
-        // animate mobile menu
-        $mainMenuState.change(function(e) {
-          var $menu = $('.block-system-main-menu .menu');
-          if (this.checked) {
-            $menu.hide().slideDown(250, function() { $menu.css('display', ''); });
-          } else {
-            $menu.show().slideUp(250, function() { $menu.css('display', ''); });
-          }
-        });
-        // hide mobile menu beforeunload
-        $(window).bind('beforeunload unload', function() {
-          if ($mainMenuState[0].checked) {
-            $mainMenuState[0].click();
-          }
-        });
-      }
-    }
-  }
-};
-
 /**
  * Override tableDragHandle().
  */
