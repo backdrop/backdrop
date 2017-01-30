e068e208a71d15703ff3dc4b74ef249b7ae0e35f
docwilmot <docwilmot@gmail.com>
2017-01-29 21:25:05 -0500

    Fix token insertion

--- core/modules/ckeditor/js/plugins/backdroptoken/plugin.js
+++ core/modules/ckeditor/js/plugins/backdroptoken/plugin.js
@@ -27,12 +27,41 @@ CKEDITOR.plugins.add('backdroptoken', {
           dialogClass: 'editor-token-dialog'
         };
 
-        // Open the dialog for the edit form.
-        console.log(editor.config.backdrop.tokenDialogUrl);
+        // Open the dialog for the token browser.
         Backdrop.ckeditor.openDialog(editor, editor.config.backdrop.tokenDialogUrl, existingValues, saveCallback, dialogSettings);
 
         $(window).on('dialog:aftercreate', function (e, dialog, $element, settings) {
-          console.log($element.find('.token-click-insert'));
+          $element.find('table.token-tree').once('token-tree', function () {
+            $(this).treetable({
+              'expandable': true
+            });
+          });
+          $element.find('.token-click-insert .token-key').once('token-click-insert', function() {
+            var newThis = $('<a href="javascript:void(0);" title="' + Backdrop.t('Insert this token into your form') + '">' + $(this).html() + '</a>').click(function(){
+            });
+            $(this).html(newThis);
+          });
+          var more = '&rtrif; ' + Backdrop.t('more');
+          var less = '&dtrif; ' + Backdrop.t('less');
+          var $link = $('<a class="token-more" href="#">' + more + ' </a>');
+          var toggleDescription = function() {
+            if ($(this).toggleClass('open').hasClass('open')) {
+              $(this).html(less).siblings('.token-description').css('display', 'block');
+            }
+            else {
+              $(this).html(more).siblings('.token-description').css('display', 'none');
+            }
+            return false;
+          }
+          $element.find('.token-description').once('token-description').each(function() {
+            var $moreLink = $link.clone();
+            $moreLink.click(toggleDescription);
+            $(this).css('display', 'none').before(' ').before($moreLink);
+          });
+          $('.token-key').once('token-key').click(function(e) {
+            var tokenText = $(this).text();
+            editor.insertText( tokenText );
+          });
         });
 
       }
