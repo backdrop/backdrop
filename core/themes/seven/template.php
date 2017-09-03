8d4f9fe91d8a525115faf21f755db984f69d3993
Jen Lampton <jen@jeneration.com>
2017-09-02 23:47:32 -0700

    Issue #2802: Seven: icon cleanup: replace old icons with lager font-awesome equivalents & rename.

--- core/themes/seven/template.php
+++ core/themes/seven/template.php
@@ -125,10 +125,10 @@ function seven_tablesort_indicator($variables) {
   $style = $variables['style'];
   $theme_path = backdrop_get_path('theme', 'seven');
   if ($style == 'asc') {
-    return theme('image', array('path' => $theme_path . '/images/arrow-asc.png', 'alt' => t('sort ascending'), 'width' => 13, 'height' => 13, 'title' => t('sort ascending')));
+    return theme('image', array('path' => $theme_path . '/images/caret-down--gray--32.png', 'alt' => t('sort ascending'), 'width' => 13, 'height' => 13, 'title' => t('sort ascending')));
   }
   else {
-    return theme('image', array('path' => $theme_path . '/images/arrow-desc.png', 'alt' => t('sort descending'), 'width' => 13, 'height' => 13, 'title' => t('sort descending')));
+    return theme('image', array('path' => $theme_path . '/images/caret-up--gray--32.png', 'alt' => t('sort descending'), 'width' => 13, 'height' => 13, 'title' => t('sort descending')));
   }
 }
 
