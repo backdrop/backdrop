bc244bb889b6021ce0306f0e89f7a545e486c758
Jen Lampton <jen@jeneration.com>
2017-09-03 18:51:14 -0700

    Replace views grid table style with CSS grid property.

--- core/modules/views/templates/views-view-grid.tpl.php
+++ core/modules/views/templates/views-view-grid.tpl.php
@@ -19,20 +19,11 @@
 <?php if (!empty($title)) : ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
-<table class="<?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
-  <?php if (!empty($caption)) : ?>
-    <caption><?php print $caption; ?></caption>
-  <?php endif; ?>
+<div class="<?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
+  <?php foreach ($rows as $row_count => $row): ?>
+    <div <?php if (!empty($row_classes[$row_count])) { print 'class="' . implode(' ', $row_classes[$row_count]) . '"';  } ?>>
+      <?php print $row; ?>
+    </div>
+  <?php endforeach; ?>
+</div>
 
-  <tbody>
-    <?php foreach ($rows as $row_number => $columns): ?>
-      <tr <?php if (!empty($row_classes[$row_number])) { print 'class="' . implode(' ', $row_classes[$row_number]) .'"';  } ?>>
-        <?php foreach ($columns as $column_number => $item): ?>
-          <td <?php if ($column_classes[$row_number][$column_number]) { print 'class="' . implode(' ', $column_classes[$row_number][$column_number]) .'"';  } ?>>
-            <?php print $item; ?>
-          </td>
-        <?php endforeach; ?>
-      </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
