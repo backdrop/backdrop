backdrop/backdrop

commit ef2be920086b216f0621738eee384373c7b00ffb

Issue #2973: Module page search should work on category names.

core/modules/system/js/modules.js
@@ -10,7 +10,7 @@ Backdrop.behaviors.moduleFilterByText = {
   attach: function(context, settings) {
     var $input = $('input.table-filter-text').once('table-filter-text');
     var $form = $('#system-modules');
-    var $rowsAndFieldsets, $rows, $fieldset;
+    var $rowsAndFieldsets, $rows, $fieldsets;
 
     // Hide the module requirements.
     $form.find('.requirements').hide();
@@ -42,8 +42,25 @@ Backdrop.behaviors.moduleFilterByText = {
       function showModuleRow(index, row) {
         var $row = $(row);
         var $sources = $row.find('.table-filter-text-source');
-        var textMatch = $sources.text().toLowerCase().indexOf(query) !== -1;
-        $row.closest('tr').toggle(textMatch);
+        var rowMatch = $sources.text().toLowerCase().indexOf(query) !== -1;
+        var $fieldsetTitle = $row.closest('fieldset').find('legend:first');
+
+        // Finding the fieldset title and filtering it can be expensive and
+        // repetitive to do for every row, so save the filtered title as data on
+        // the DOM element.
+        var filterTitle;
+        if (!$fieldsetTitle.data('filterTitle')) {
+          // Don't include hidden DOM elements such as the show/hide label.
+          filterTitle = $fieldsetTitle.clone().find('.element-invisible').remove().end().text().toLowerCase();
+          $fieldsetTitle.data('filterTitle', filterTitle);
+        }
+        else {
+          filterTitle = $fieldsetTitle.data('filterTitle');
+        }
+        var fieldsetTitleMatch = filterTitle.indexOf(query) !== -1;
+
+        // If the row contains the string or the fieldset does, show the row.
+        $row.closest('tr').toggle(rowMatch || fieldsetTitleMatch);
       }
 
       // Filter only if the length of the query is at least 2 characters.
@@ -52,8 +69,10 @@ Backdrop.behaviors.moduleFilterByText = {
 
         // We first show() all <fieldset>s to be able to use ':visible'.
         $fieldset.show().each(hidePackageFieldset);
+        // Then show modules when the package matches.
+        $fieldset.each(showPackageFieldset);
 
-        if ($fieldset.filter(':visible').length === 0) {
+        if ($fieldsets.filter(':visible').length === 0) {
           if ($('.filter-empty').length === 0) {
             $('#edit-filter').append('<p class="filter-empty">' + Backdrop.t('There were no results.') + '</p>');
           }
@@ -71,7 +90,7 @@ Backdrop.behaviors.moduleFilterByText = {
     if ($form.length) {
       $rowsAndFieldsets = $form.find('tr, fieldset');
       $rows = $form.find('tbody tr');
-      $fieldset = $form.find('fieldset');
+      $fieldsets = $form.find('fieldset');
 
       // @todo Use autofocus attribute when possible.
       $input.focus().on('keyup', filterModuleList);

commit a9b1b163f39f09967cdd4a38d01f48467a58605a

Issue #2973, Cleanup JS errors introduced in ef2be92008

core/modules/system/js/modules.js
@@ -68,9 +68,7 @@ Backdrop.behaviors.moduleFilterByText = {
         $rows.each(showModuleRow);
 
         // We first show() all <fieldset>s to be able to use ':visible'.
-        $fieldset.show().each(hidePackageFieldset);
-        // Then show modules when the package matches.
-        $fieldset.each(showPackageFieldset);
+        $fieldsets.show().each(hidePackageFieldset);
 
         if ($fieldsets.filter(':visible').length === 0) {
           if ($('.filter-empty').length === 0) {