e97cf80b32c2747315e209f7ed677189e5492f92
docwilmot <docwilmot@users.noreply.github.com>
2018-06-30 19:12:58 +0100

    Prevent clicks on links during preview

--- core/modules/node/js/node.preview.js
+++ core/modules/node/js/node.preview.js
@@ -3,9 +3,25 @@
 
 Backdrop.behaviors.nodePreview = {
   attach: function (context) {
+    // Insert the preview admin banner into the page before the layout.
     $('.node-preview-container').each(function() {
       $(this).insertBefore($('.page').find('.layout'));
     });
+    // Prevent clicking on links during preview.
+    var $preview = $(context).find('.layout').once('layout-preview');
+    if ($preview.length) {
+      $preview.on('click.preview', 'a', function (e) {
+        e.preventDefault();
+      });
+    }
+  },
+  detach: function (context, settings, trigger) {
+    if (trigger === 'unload') {
+      var $preview = $(context).find('.layout').removeOnce('layout-preview');
+      if ($preview.length) {
+        $preview.off('click.preview');
+      }
+    }
   }
 };
 
