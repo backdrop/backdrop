223f11af6c933cad360b107a19b9b66e394053fa
docwilmot <docwilmot@users.noreply.github.com>
2018-07-01 08:52:29 +0100

    Fix JS filters

--- core/modules/system/js/modules.js
+++ core/modules/system/js/modules.js
@@ -1,16 +1,14 @@
 (function ($) {
 
 /**
- * Filters the module list table by a text input search string.
- *
- * Additionally accounts for multiple tables being wrapped in "package" fieldset
- * elements.
+ * Filters the module list table by a text input search string, tags, and
+ * module source (core, contrib or custom).
  */
 Backdrop.behaviors.moduleFilter = {
   attach: function(context, settings) {
     var $input = $('input.table-filter-text').once('table-filter-text');
     var $form = $('#system-modules');
-    var $rowsAndFieldsets, $rows, $fieldsets;
+    var $allRows, $rows;
 
     // Hide the module requirements.
     $form.find('.requirements').hide();
@@ -28,14 +26,7 @@ Backdrop.behaviors.moduleFilter = {
       e.stopPropagation();
     });
 
-    // Hide the package <fieldset> if it doesn't have any visible rows within.
-    function hidePackageFieldset(index, element) {
-      var $fieldset = $(element);
-      var $visibleRows = $fieldset.find('table:not(.sticky-header)').find('tbody tr:visible');
-      $fieldset.toggle($visibleRows.length > 0);
-    }
-
-    // Fliter the list of modules by provided search string.
+    // Filter the list of modules by provided search string.
     function filterModuleList() {
       var query = $input.val().toLowerCase();
       var tag = $('select[data-filter-tags]').val();
@@ -47,24 +38,7 @@ Backdrop.behaviors.moduleFilter = {
         var $row = $(row);
         var $sources = $row.find('.table-filter-text-source');
         var rowMatch = $sources.text().toLowerCase().indexOf(query) !== -1;
-        var $fieldsetTitle = $row.closest('fieldset').find('legend:first');
         var tagSource = $row.find('.module-tags').text().toLowerCase();
-
-        // Finding the fieldset title and filtering it can be expensive and
-        // repetitive to do for every row, so save the filtered title as data on
-        // the DOM element.
-        var filterTitle;
-        if (!$fieldsetTitle.data('filterTitle')) {
-          // Don't include hidden DOM elements such as the show/hide label.
-          filterTitle = $fieldsetTitle.clone().find('.element-invisible').remove().end().text().toLowerCase();
-          $fieldsetTitle.data('filterTitle', filterTitle);
-        }
-        else {
-          filterTitle = $fieldsetTitle.data('filterTitle');
-        }
-        // Compare the search query to the fieldset title.
-        var fieldsetTitleMatch = filterTitle.indexOf(query) !== -1;
-        // Compare the requested tag to each row's tags.
         var tagMatch = tagSource.indexOf(tag) !== -1;
 
         // Check the module souce and show only if matched.
@@ -79,8 +53,8 @@ Backdrop.behaviors.moduleFilter = {
           sourceMatch = true;
         }
 
-        // If the row contains the string or the fieldset does, show the row.
-        $row.closest('tr').toggle((rowMatch || fieldsetTitleMatch) && tagMatch && sourceMatch);
+        // If the row contains the string show the row.
+        $row.closest('tr').toggle((rowMatch) && tagMatch && sourceMatch);
       }
 
       // Filter only if the length of the search query is at least 2 characters.
@@ -91,10 +65,7 @@ Backdrop.behaviors.moduleFilter = {
 
         $rows.each(showModuleRow);
 
-        // We first show() all <fieldset>s to be able to use ':visible'.
-        $fieldsets.show().each(hidePackageFieldset);
-
-        if ($fieldsets.filter(':visible').length === 0) {
+        if ($rows.filter(':visible').length === 0) {
           if ($('.filter-empty').length === 0) {
             $('#edit-filter').append('<p class="filter-empty">' + Backdrop.t('There were no results.') + '</p>');
           }
@@ -104,15 +75,14 @@ Backdrop.behaviors.moduleFilter = {
         }
       }
       else {
-        $rowsAndFieldsets.show();
+        $allRows.show();
         $('.filter-empty').remove();
       }
     }
 
     if ($form.length) {
-      $rowsAndFieldsets = $form.find('tr, fieldset');
+      $allRows = $form.find('tr');
       $rows = $form.find('tbody tr');
-      $fieldsets = $form.find('fieldset');
 
       // @todo Use autofocus attribute when possible.
       $input.focus().on('keyup', filterModuleList);
