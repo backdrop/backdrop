ac29cb7b1213f42ea5f4fa04696ff7ec8c575bf8
Graham Oliver <g.oliver@btinternet.com>
2018-07-11 19:45:38 +0100

    Modify Image Library View used by Filter module.

---
+++ core/themes/seven/templates/views-view-field--image-library--nothing.tpl.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @file
+ * This template is used to print a single field in a view.
+ *
+ * It is not actually used in default Views, as this is registered as a theme
+ * function which has better performance. For single overrides, the template is
+ * perfectly okay.
+ *
+ * Variables available:
+ * - $view: The view object
+ * - $field: The field handler object that can process the input
+ * - $row: The raw SQL result that can be used
+ * - $output: The processed output that will normally be used.
+ *
+ * When fetching output from the $row, this construct should be used:
+ * $data = $row->{$field->field_alias}
+ *
+ * The above will guarantee that you'll always get the correct data,
+ * regardless of any changes in the aliasing that might happen if
+ * the view is modified.
+ */
+?>
+<?php
+// $output contains URI of image.
+// Find position of '/files'.
+$insert_at = strpos( $output, '/files');
+// Text to insert.
+$add_to_uri = '/styles/thumbnail_150/public';
+$new_output = substr_replace ($output, $add_to_uri, $insert_at + 6,0 );
+print ($new_output); ?>
