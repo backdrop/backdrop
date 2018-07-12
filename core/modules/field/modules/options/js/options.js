8db7f1f0e1cd3ced82229c2be069ad5018ed3969
Herb vd Dool <herb@freeform.ca>
2018-07-12 09:42:52 -0400

    fix toggle of default checkbox/radio

--- core/modules/field/modules/options/js/options.js
+++ core/modules/field/modules/options/js/options.js
@@ -819,12 +819,21 @@ Backdrop.theme.tableDragChangedWarning = function() {
  */
 Backdrop.behaviors.optionsElementFieldUI = {};
 Backdrop.behaviors.optionsElementFieldUI.attach = function(context) {
+  var $cardinalityFieldNumber = $(context).find('#edit-field-cardinality-number');
+  if ($cardinalityFieldNumber.length) {
+    $cardinalityFieldNumber.change(function() {
+      var optionsElementId = $(this).parents('fieldset:first').find('.form-type-options table').attr('id');
+      if (Backdrop.optionElements[optionsElementId]) {
+        Backdrop.optionElements[optionsElementId].setMultiple(this.value != 1);
+      }
+    }).trigger('change');
+  }
   var $cardinalityField = $(context).find('#edit-field-cardinality');
   if ($cardinalityField.length) {
     $cardinalityField.change(function() {
       var optionsElementId = $(this).parents('fieldset:first').find('.form-type-options table').attr('id');
       if (Backdrop.optionElements[optionsElementId]) {
-        Backdrop.optionElements[optionsElementId].setMultiple(this.value != 1);
+        Backdrop.optionElements[optionsElementId].setMultiple(this.value == -1);
       }
     }).trigger('change');
   }
