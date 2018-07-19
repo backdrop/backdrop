19b2bd1cc63a4c5a2d1a037ada6e90fd5c1ca034
Alex Finnarn <alexfinnarn@gmail.com>
2018-07-19 10:08:26 -0400

    adding link to change record

--- core/modules/block/block.api.php
+++ core/modules/block/block.api.php
@@ -1,7 +1,12 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: alfi2595
- * Date: 7/19/18
- * Time: 10:02 AM
+ * @file
+ * API hooks for the block module.
+ */
+
+/**
+ * NOTE: The block module's hooks are now implemented in the layout module.
+ * Please look in layout.api.php to find them.
+ * You can read more in the change record: https://api.backdropcms.org/node/26861
  */
