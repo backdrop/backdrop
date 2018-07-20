6c2f870f8fc7a9c7d96affe19122aa13d65c6145
Geoff St. Pierre <serundeputy@gmail.com>
2018-07-20 17:17:26 -0400

    Adding core/misc/zenci/run_phpcs.sh to run phpcs on backdrop/backdrop PRs.

---
+++ core/misc/zen-ci/run_phpcs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+##
+# Run phpcs code standards tests.
+##
+
+#set site path
+SITEPATH="$HOME/www"
+
+# Go to domain directory.
+cd $SITEPATH
+
+# Install and run backdrop/coder
+composer require backdrop/coder
+./vendor/bin/phpcs --standard=./vendor/backdrop/coder/coder_sniffer/Backdrop core/modules
