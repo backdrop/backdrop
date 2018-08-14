b3cc99fc92f2b27b3d205f11b143b1a7881a7c3e
quicksketch <nate@quicksketch.org>
2018-08-14 15:37:21 -0700

    Additional docs.

--- core/misc/zen-ci/run_phpcs.sh
+++ core/misc/zen-ci/run_phpcs.sh
@@ -2,9 +2,12 @@
 
 ##
 # Run phpcs code standards tests.
+#
+# This utilizes the PHP_CodeSniffer (phpcs) library included as part of the
+# contrib "coder" module.
 ##
 
-#set site path
+# Set site path
 SITEPATH="$HOME/www"
 
 # Go to domain directory.
