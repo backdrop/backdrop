fe5e1e43426796dbffaf44cf50668854a686efcf
quicksketch <nate@quicksketch.org>
2018-08-30 12:24:36 -0700

    Capitalizing

--- core/modules/block/block.api.php
+++ core/modules/block/block.api.php
@@ -2,11 +2,11 @@
 
 /**
  * @file
- * API hooks for the block module.
+ * API hooks for the Block module.
  */
 
 /**
- * NOTE: The block module's hooks are now implemented in the layout module.
+ * NOTE: The Block module's hooks are now implemented in the Layout module.
  * Please look in layout.api.php to find them.
  * You can read more in the change record: https://api.backdropcms.org/node/26861
  */
