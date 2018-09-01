6707e9cdb5e14c2ad0e31a0ee5e8c199bbd6f259
docwilmot <docwilmot@users.noreply.github.com>
2018-09-01 07:10:40 -0500

    Better JS

--- core/modules/node/js/node.preview.js
+++ core/modules/node/js/node.preview.js
@@ -4,16 +4,15 @@
 Backdrop.behaviors.nodePreview = {
   attach: function (context) {
     // Insert the preview admin banner into the page before the layout.
-    $('.node-preview-container').each(function() {
-      $(this).insertBefore($('.page').once('preview-js').find('.layout'));
+    $('.node-preview-container').once('node-preview').each(function() {
+      $topParent = $('body').find('div').first();
+      $topParent.addClass('preview-js');
+      $(this).insertBefore($topParent);
     });
     // Prevent clicking on links during preview.
-    var $preview = $(context).find('.layout').once('layout-preview');
-    if ($preview.length) {
-      $preview.on('click.preview', 'a', function (e) {
-        e.preventDefault();
-      });
-    }
+    $topParent.on('click.preview', 'a', function (e) {
+      e.preventDefault();
+    });
   },
   detach: function (context, settings, trigger) {
     if (trigger === 'unload') {
