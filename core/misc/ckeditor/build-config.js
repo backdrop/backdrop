e619e6afc9dfe4a8fdc6583d12faa9cdb0a77fcd
Gregory Netsas <gregory@salsadigital.com.au>
2018-11-08 06:22:18 +1100

    Add Backdrop customizations in build-config.js

--- core/misc/ckeditor/build-config.js
+++ core/misc/ckeditor/build-config.js
@@ -1,6 +1,18 @@
 ﻿/**
- * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
+ * This is a Backdrop-optimized build of CKEditor.
+ *  
+ * You may re-use it at any time at http://ckeditor.com/builder to build  
+ * CKEditor again. Alternatively, use the "build.sh" script to build it locally.  
+ * If you do so, be sure to pass it the "-s" flag. So: "sh build.sh -s".  
+ *  
+ * NOTE:  
+ *    This file is not used by CKEditor, you may remove it. Changing this file
+ *    will not change your CKEditor configuration. 
+ */
+
+/**
+ * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights
+ * reserved. For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
 /**
@@ -14,10 +26,12 @@
  *     Visit online builder to build CKEditor from scratch.
  *
  * (2) http://ckeditor.com/builder/58989eabf7abd2158a32dcce585c9e95
- *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *     Visit online builder to build CKEditor, starting with the same setup as
+ *     before.
  *
  * (3) http://ckeditor.com/builder/download/58989eabf7abd2158a32dcce585c9e95
- *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *     Straight download link to the latest version of CKEditor (Optimized) with
+ *     the same setup as before.
  *
  * NOTE:
  *    This file is not used by CKEditor, you may remove it.
@@ -52,6 +66,16 @@ var CKBUILDER_CONFIG = {
 		'package.json',
 		'tests'
 	],
+  // So that they are not shipped with Backdrop, after using this config file 
+  // for rebuilding, the following are removed manually, because adding them  
+  // to the ignore array does not cause them to be ignored: 
+  // 'README.md', 
+  // 'adapters',  
+  // 'config.js', 
+  // 'contents.css',  
+  // 'samples', 
+  // 'skins/moono-lisa/readme.md',  
+  // 'styles.js',
 	plugins : {
 		'a11yhelp' : 1,
 		'about' : 1,
