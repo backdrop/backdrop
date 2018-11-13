0b32533cbef28d4e29f7f42d24ad9fa911f1b4da
Gregory Netsas <gregory@userfriendly.tech>
2018-11-14 07:48:17 +1100

    Update settings.php

--- settings.php
+++ settings.php
@@ -314,6 +314,39 @@ $settings['404_fast_html'] = '<!DOCTYPE html><html><head><title>404 Not Found</t
 // $settings['allow_authorize_operations'] = FALSE;
 
 /**
+ * Trusted host configuration.
+ *
+ * Backdrop core can use a trusted host mechanism to prevent HTTP Host header
+ * spoofing. For more information, see https://www.drupal.org/node/1992030
+ *
+ * To enable the trusted host mechanism, you specify your allowable hosts
+ * in $settings['trusted_host_patterns'] in your settings.php file. This should
+ * be an array of regular expression patterns, without delimiters, representing
+ * the hosts you would like to allow.
+ *
+ * For example, this will allow the site to only run from www.example.com:
+ * @code
+ * $settings['trusted_host_patterns'] = array(
+ *   '^www\.example\.com$',
+ * );
+ * @endcode
+ *
+ * If you are running a multisite, or if you are running your site from
+ * different domain names (eg, you don't redirect http://www.example.com to
+ * http://example.com), you should specify all of the host patterns that are
+ * allowed by your site. For example, this will allow the site to run off of all
+ * variants of example.com and example.org, with all subdomains included:
+ * @code
+ * $settings['trusted_host_patterns'] = array(
+ *   '^example\.com$',
+ *   '^.+\.example\.com$',
+ *   '^example\.org',
+ *   '^.+\.example\.org',
+ * );
+ * @endcode
+*/
+
+/**
  * Mixed-mode sessions:
  *
  * Set to TRUE to create both secure and insecure sessions when using HTTPS.
