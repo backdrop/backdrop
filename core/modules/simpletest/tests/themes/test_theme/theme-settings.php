807a9e5becefe64711bb0017895210f24a397753
Gregory Netsas <gregory@userfriendly.tech>
2018-11-24 23:38:35 +1100

    ...remove redundant comment about hook_form_FORM_ID_alter()

--- core/modules/simpletest/tests/themes/test_theme/theme-settings.php
+++ core/modules/simpletest/tests/themes/test_theme/theme-settings.php
@@ -3,15 +3,12 @@
  * @file
  * Theme settings file for the test_theme theme.
  */
- /**
- * Implements hook_form_FORM_ID_alter().
- */
 $form['test_theme_checkbox'] = array(
   '#type' => 'checkbox',
   '#title' => 'Test theme checkbox',
   '#default_value' => theme_get_setting('test_theme_checkbox', 'test_theme'),
 );
- // Force the form to be cached so we can test that this file is properly
+// Force the form to be cached so we can test that this file is properly
 // loaded and the custom submit handler is properly called even on a cached
 // form build.
 $form_state['cache'] = TRUE;
