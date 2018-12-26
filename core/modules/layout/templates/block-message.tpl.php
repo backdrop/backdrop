1d06a51dc3415ade6e9c8722b2004cf5820833cc
Gregory Netsas <gregory@userfriendly.tech>
2018-12-26 17:30:04 +0200

    ...fix variables docblock section

--- core/modules/layout/templates/block-message.tpl.php
+++ core/modules/layout/templates/block-message.tpl.php
@@ -11,24 +11,14 @@
  * site-builders to configure text blocks to be rendered as messages, through
  * the user interface.
  *
- * Variables available:
- * - $message_type: The HTML tag to be used around the entire block.
- * - $classes: Array of classes that should be displayed on the block's wrapper.
+ * * Variables available:
+ * - $message_type: 'info', 'status', 'warning', or 'error' - set from the user
+ *   interface.
  * - $attributes: Attributes that should be displayed on this block's wrapper.
- * - $title: The title of the block.
- * - $title_prefix/$title_suffix: A prefix and suffix for the title tag. This
- *   is important to print out as administrative links to edit this block are
- *   printed in these variables.
- * - $title_tag: The HTML tag to be used on the title of the block.
- * - $content_tag: The HTML tag to be used around the content.
- * - $content_attributes: Attributes that should be displayed on the content.
  * - $content: The actual content of the block.
  */
 ?>
-<?php if ($message_type): ?>
-  <div class="message-block messages <?php print $message_type; ?>"<?php print backdrop_attributes($attributes); ?>>
-<?php endif; ?>
-
+<div class="message-block messages <?php print $message_type; ?>"<?php print backdrop_attributes($attributes); ?>>
   <div class="block-content">
     <?php print render($content); ?>
   </div>
