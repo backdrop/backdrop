60a38321cdbfd2e1c8caba0e0ae54e640c125d82
Gregory Netsas <gregory@userfriendly.tech>
2019-03-25 20:04:36 +1100

    Add fieldset summary

---
+++ core/modules/system/js/themes.js
@@ -0,0 +1,29 @@
+/**
+ * Provides summary text for the "Administration theme" fieldset, in the "List
+ * themes" page (/admin/appearance).
+ */
+ (function ($) {
+
+"use strict";
+
+Backdrop.behaviors.systemFieldsetSummaries = {
+  attach: function (context) {
+    $(context).find('fieldset.admin-theme-form').backdropSetSummary(function (element) {
+      var $element = $(element);
+      var admin_theme  = $element.find('[name="admin_theme"]').val();
+      var node_admin_theme = $element.find('node_admin_theme').prop('checked');
+
+      if (node_admin_theme) {
+        return Backdrop.t('@admin_theme - also used when editing or creating content', { '@admin_theme': admin_theme });
+      }
+      if (admin_theme == 'Default theme') {
+        return Backdrop.t('Same theme as the rest of the site');
+      }
+      else {
+        return admin_theme;
+      }
+    });
+  }
+};
+
+})(jQuery);
