837812736e150433b4abb8a38b4a3834360f86a2
Gregory Netsas <gregory@userfriendly.tech>
2019-03-25 20:39:43 +1100

    ...fix fieldset summaries

--- core/modules/system/js/themes.js
+++ core/modules/system/js/themes.js
@@ -11,12 +11,12 @@ Backdrop.behaviors.systemFieldsetSummaries = {
     $(context).find('fieldset.admin-theme-form').backdropSetSummary(function (element) {
       var $element = $(element);
       var admin_theme  = $element.find('[name="admin_theme"]').val();
-      var node_admin_theme = $element.find('node_admin_theme').prop('checked');
+      var node_admin_theme = $element.find('[name="node_admin_theme"]').prop('checked');
 
       if (node_admin_theme) {
         return Backdrop.t('@admin_theme - also used when editing or creating content', { '@admin_theme': admin_theme });
       }
-      if (admin_theme == 'Default theme') {
+      if (admin_theme == 0) {
         return Backdrop.t('Same theme as the rest of the site');
       }
       else {
