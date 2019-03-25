b30d63befe3ae9329fb454a81bd7164b05cfc005
Gregory Netsas <gregory@userfriendly.tech>
2019-03-25 21:05:56 +1100

    ...fix fieldset summary some more

--- core/modules/system/js/themes.js
+++ core/modules/system/js/themes.js
@@ -11,16 +11,17 @@ Backdrop.behaviors.systemFieldsetSummaries = {
     $(context).find('fieldset.admin-theme-form').backdropSetSummary(function (element) {
       var $element = $(element);
       var admin_theme  = $element.find('[name="admin_theme"]').val();
+      var admin_theme_name  = $element.find('[name="admin_theme"] :selected').text();
       var node_admin_theme = $element.find('[name="node_admin_theme"]').prop('checked');
 
-      if (node_admin_theme) {
-        return Backdrop.t('@admin_theme - also used when editing or creating content', { '@admin_theme': admin_theme });
+      if (node_admin_theme && admin_theme != 0) {
+        return Backdrop.t('@theme_name - also used when editing or creating content', { '@theme_name': admin_theme_name });
       }
       if (admin_theme == 0) {
         return Backdrop.t('Same theme as the rest of the site');
       }
       else {
-        return admin_theme;
+        return admin_theme_name;
       }
     });
   }
