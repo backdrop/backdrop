74fc448ddc524014d22af8ccaeaa729ff3f1dd67
Gregory Netsas <gregory@twinz.gr>
2019-08-02 20:04:39 +0300

    Issue #3720: Views Plugins documentation topic lists incorrect number of plugin types.
    Cross-port of Drupal Issue #2408939 by shashikant_chauhan: Views Plugins documentation topic lists incorrect number of plugin types.
    
    Port by @klonos.

--- core/modules/views/views.api.php
+++ core/modules/views/views.api.php
@@ -139,7 +139,7 @@
  * responsible for building the query. Instead, they are objects that are used
  * to display the view or make other modifications.
  *
- * There are 10 types of plugins in Views:
+ * There are several types of plugins in Views:
  * - Display: Display plugins are responsible for controlling *where* a view
  *   lives; that is, how they are being exposed to other parts of Backdrop. Page
  *   and block are the most common displays, as well as the ubiquitous 'master'
