87241d07aa2333c839720b2301e02f09b9819166
docwilmot <docwilmot@users.noreply.github.com>
2020-02-16 09:23:16 -0500

    Start clean JS

--- core/modules/filter/js/filter.admin.js
+++ core/modules/filter/js/filter.admin.js
@@ -7,41 +7,20 @@
 
 Backdrop.behaviors.filterStatus = {
   attach: function (context, settings) {
-    $('#filters-status-wrapper input.form-checkbox', context).once('filter-status', function () {
-      var $checkbox = $(this);
-      // Retrieve the tabledrag row belonging to this filter.
-      var $row = $('#' + $checkbox.attr('id').replace(/-status$/, '-weight'), context).closest('tr');
-      // Retrieve the vertical tab belonging to this filter.
-      var tab = $('#' + $checkbox.attr('id').replace(/-status$/, '-settings'), context).data('verticalTab');
+    // TableDrag is required and we should be on the filters admin page.
+    if (typeof Backdrop.tableDrag == 'undefined' || typeof Backdrop.tableDrag.filterorder == 'undefined') {
+      return;
+    }
 
-      // Bind click handler to this checkbox to conditionally show and hide the
-      // filter's tableDrag row and vertical tab pane.
-      $checkbox.bind('click.filterUpdate', function () {
-        if ($checkbox.is(':checked')) {
-          $row.show();
-          if (tab) {
-            tab.tabShow().updateSummary();
-          }
-        }
-        else {
-          $row.hide();
-          if (tab) {
-            tab.tabHide().updateSummary();
-          }
-        }
-        // Restripe table after toggling visibility of table row.
-        Backdrop.tableDrag['filter-order'].restripeTable();
-      });
+    // A custom message for the filters page specifically.
+    Backdrop.theme.tableDragChangedWarning = function () {
+      return '<div class="messages warning">' + Backdrop.theme('tableDragChangedMarker') + ' ' + Backdrop.t('The changes to these filters will not be saved until the <em>Save configuration</em> button is clicked.') + '</div>';
+    };
 
-      // Attach summary for configurable filters (only for screen-readers).
-      if (tab) {
-        tab.fieldset.backdropSetSummary(function (tabContext) {
-          return $checkbox.is(':checked') ? Backdrop.t('Enabled') : Backdrop.t('Disabled');
-        });
-      }
-
-      // Trigger our bound click handler to update elements to initial state.
-      $checkbox.triggerHandler('click.filterUpdate');
+    // Toggle class on row if checkbox changed.
+    $('#filterorder input.filter-status:checkbox', context).change(function (event) {
+        var isChecked = !$(this).prop('checked');
+        $(this).closest('tr').toggleClass('disabled-row', isChecked);
     });
   }
 };
