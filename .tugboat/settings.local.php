d6724cb501fdcd282955acb145f8fa20a2becf35
BWPanda <peter@panda.id.au>
2020-03-21 22:17:30 +0200

    Allow Tugboat-specific settings.

---
+++ .tugboat/settings.local.php
@@ -0,0 +1,8 @@
+<?php
+// Database.
+$database = 'mysql://tugboat:tugboat@mysql/tugboat';
+// $database_prefix = '';
+// $database_charset = 'utf8mb4';
+
+// Trusted hosts.
+$settings['trusted_host_patterns'] = array('^.+\.tugboat\.qa$');
