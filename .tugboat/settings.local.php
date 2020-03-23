3bcfa7a230d0ac36807538e40440a40a6f98953b
BWPanda <peter@panda.id.au>
2020-03-23 21:58:30 +0200

    Simplifying code, fixing permissions.

--- .tugboat/settings.local.php
+++ .tugboat/settings.local.php
@@ -1,8 +1,9 @@
 <?php
 // Database.
 $database = 'mysql://tugboat:tugboat@mariadb/tugboat';
-// $database_prefix = '';
 // $database_charset = 'utf8mb4';
 
 // Trusted hosts.
 $settings['trusted_host_patterns'] = array('^.+\.tugboat\.qa$');
+
+// Miscellaneous.
