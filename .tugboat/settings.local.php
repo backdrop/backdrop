50b7acec39b47095eab559dd6bdc3e121a4cdfed
BWPanda <peter@panda.id.au>
2020-03-23 22:49:13 +0200

    Support UTF-8 and clean URLs.

--- .tugboat/settings.local.php
+++ .tugboat/settings.local.php
@@ -1,7 +1,7 @@
 <?php
 // Database.
 $database = 'mysql://tugboat:tugboat@mariadb/tugboat';
-// $database_charset = 'utf8mb4';
+$database_charset = 'utf8mb4';
 
 // Trusted hosts.
 $settings['trusted_host_patterns'] = array('^.+\.tugboat\.qa$');
