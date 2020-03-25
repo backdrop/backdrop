ddb15aecc74b4715c549bdc4807dae4a19a7303a
BWPanda <peter@panda.id.au>
2020-03-25 21:15:57 +0200

    Fix environment variables.

--- .tugboat/post-comment.sh
+++ .tugboat/post-comment.sh
@@ -4,32 +4,26 @@
  * Post a comment on the PR with login details.
  */
 
-$comment = "Tugboat has finished building a preview for this pull request!\n\n
-Website: $TUGBOAT_DEFAULT_SERVICE_URL\n
-Username: admin\n
-Password: password";
+$comment = 'Tugboat has finished building a preview for this pull request!
+
+Website: ' . getenv('TUGBOAT_DEFAULT_SERVICE_URL') . '
+Username: admin
+Password: password';
 
 // Initialise session.
-$ch = curl_init("https://api.github.com/repos/$TUGBOAT_GITHUB_OWNER/$TUGBOAT_GITHUB_REPO/issues/$TUGBOAT_GITHUB_PR/comments");
+$ch = curl_init('https://api.github.com/repos/' . getenv('TUGBOAT_GITHUB_OWNER') . '/' . getenv('TUGBOAT_GITHUB_REPO') . '/issues/' . getenv('TUGBOAT_GITHUB_PR') . '/comments');
 
 // Set options.
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-  'Authorization: token ' . $BACKDROP_GITHUB_TOKEN,
+  'Authorization: token ' . getenv('BACKDROP_GITHUB_TOKEN'),
   'Content-Type: application/json',
   'Accept: application/vnd.github.v3+json',
   'User-Agent: Backdrop CMS',
 ));
-curl_setopt($ch, CURLOPT_HEADER, TRUE);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 curl_setopt($ch, CURLOPT_POST, TRUE);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array('body' => $comment)));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 
-// Execute and parse response.
-$response = curl_exec($ch);
-// $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-// dpm($code = curl_getinfo($ch, CURLINFO_HTTP_CODE));
-// $header = trim(substr($response, 0, $header_size));
-// dpm($body = json_decode(substr($response, $header_size)));
-
-// Close session.
+// Execute, then close session.
+curl_exec($ch);
 curl_close($ch);
