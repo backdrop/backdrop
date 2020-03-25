3720e71ea32b600b3a194993f5c13562a7963b4a
BWPanda <peter@panda.id.au>
2020-03-25 14:21:04 +0200

    Updating with latest code.

--- .tugboat/settings.local.php
+++ .tugboat/settings.local.php
@@ -3,6 +3,10 @@
 $database = 'mysql://tugboat:tugboat@mariadb/tugboat';
 $database_charset = 'utf8mb4';
 
+// Config.
+$config_directories['active'] = 'files/config/active';
+$config_directories['staging'] = 'files/config/staging';
+
 // Trusted hosts.
 $settings['trusted_host_patterns'] = array('^.+\.tugboat\.qa$');
 
