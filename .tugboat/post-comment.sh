b9334e12b36e1ea03ae63459bded2b8687fdce67
BWPanda <peter@panda.id.au>
2020-03-25 22:24:56 +0200

    Delete previous comments first.

--- .tugboat/post-comment.sh
+++ .tugboat/post-comment.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 /**
- * Post a comment on the PR with login details.
+ * Post a comment in the PR with login details.
  */
 
 $comment = 'Tugboat has finished building a preview for this pull request!
@@ -10,10 +10,8 @@ Website: ' . getenv('TUGBOAT_DEFAULT_SERVICE_URL') . '
 Username: admin
 Password: password';
 
-// Initialise session.
+// Post comment to GitHub.
 $ch = curl_init('https://api.github.com/repos/' . getenv('TUGBOAT_GITHUB_OWNER') . '/' . getenv('TUGBOAT_GITHUB_REPO') . '/issues/' . getenv('TUGBOAT_GITHUB_PR') . '/comments');
-
-// Set options.
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
   'Authorization: token ' . getenv('BACKDROP_GITHUB_TOKEN'),
   'Content-Type: application/json',
@@ -23,7 +21,5 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 curl_setopt($ch, CURLOPT_POST, TRUE);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array('body' => $comment)));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-
-// Execute, then close session.
 curl_exec($ch);
 curl_close($ch);
