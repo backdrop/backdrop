2c64fda01907e427964f760adaf43d17c847c46d
indigoxela <user@olga.indigofloat.at>
2020-05-06 16:04:00 +0200

    Issue #4111: php warning headers already sent

--- settings.php
+++ settings.php
@@ -151,29 +151,31 @@ $settings['hash_salt'] = '';
  * defined there should not be duplicated here so as to avoid conflict issues.
  */
 
-/**
- * Some distributions of Linux (most notably Debian) ship their PHP
- * installations with garbage collection (gc) disabled. Since Backdrop depends
- * on PHP's garbage collection for clearing sessions, ensure that garbage
- * collection occurs by using the most common settings.
- */
-ini_set('session.gc_probability', 1);
-ini_set('session.gc_divisor', 100);
+if (!backdrop_is_cli()) {
+  /**
+   * Some distributions of Linux (most notably Debian) ship their PHP
+   * installations with garbage collection (gc) disabled. Since Backdrop depends
+   * on PHP's garbage collection for clearing sessions, ensure that garbage
+   * collection occurs by using the most common settings.
+   */
+  ini_set('session.gc_probability', 1);
+  ini_set('session.gc_divisor', 100);
 
-/**
- * Set session lifetime (in seconds), i.e. the time from the user's last visit
- * to the active session may be deleted by the session garbage collector. When
- * a session is deleted, authenticated users are logged out, and the contents
- * of the user's $_SESSION variable is discarded.
- */
-ini_set('session.gc_maxlifetime', 200000);
+  /**
+   * Set session lifetime (in seconds), i.e. the time from the user's last visit
+   * to the active session may be deleted by the session garbage collector. When
+   * a session is deleted, authenticated users are logged out, and the contents
+   * of the user's $_SESSION variable is discarded.
+   */
+  ini_set('session.gc_maxlifetime', 200000);
 
-/**
- * Set session cookie lifetime (in seconds), i.e. the time from the session is
- * created to the cookie expires, i.e. when the browser is expected to discard
- * the cookie. The value 0 means "until the browser is closed".
- */
-ini_set('session.cookie_lifetime', 2000000);
+  /**
+   * Set session cookie lifetime (in seconds), i.e. the time from the session is
+   * created to the cookie expires, i.e. when the browser is expected to discard
+   * the cookie. The value 0 means "until the browser is closed".
+   */
+  ini_set('session.cookie_lifetime', 2000000);
+}
 
 /**
  * If you encounter a situation where users post a large amount of text, and
