2830408fcc444dfee42a327ba12535073b6fbb46
Jen Lampton <jen@jeneration.com>
2020-06-15 12:23:32 -0700

    Issue #4441: Remove the words blacklist and whitelist from core.

--- core/modules/filter/js/filter.admin.js
+++ core/modules/filter/js/filter.admin.js
@@ -316,8 +316,8 @@ Backdrop.FilterStatus.prototype.addHTMLRule = function (rule) {
  * Backdrop.FilterStatus.
  *
  * A text filter rule object describes
- *  1. allowed or forbidden tags: (optional) whitelist or blacklist HTML tags
- *  2. restricted tag properties: (optional) whitelist or blacklist attributes,
+ *  1. allowed or forbidden tags: (optional) allowlist or denylist HTML tags
+ *  2. restricted tag properties: (optional) allowlist or denylist attributes,
  *     styles and classes on a set of HTML tags.
  *
  * Typically, each text filter rule object does either 1 or 2, not both.
@@ -329,11 +329,11 @@ Backdrop.FilterStatus.prototype.addHTMLRule = function (rule) {
  *     restrictions are applied.
  *  2. all nested within the "properties" key: use the "tags" subkey to list
  *     HTML tags to which you want to apply property restrictions, then use the
- *     "allowed" subkey to whitelist specific property values, and similarly use
- *     the "forbidden" subkey to blacklist specific property values.
+ *     "allowed" subkey to allowlist specific property values, and similarly use
+ *     the "forbidden" subkey to denylist specific property values.
  *
  * Examples:
- *  - Whitelist the "p", "strong" and "a" HTML tags:
+ *  - Allow the "p", "strong" and "a" HTML tags:
  *    {
  *      tags: ['p', 'strong', 'a'],
  *      allow: true,
