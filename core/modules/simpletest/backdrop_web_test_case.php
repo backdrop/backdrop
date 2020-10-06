e819057cf23bec6248c15424dc687284fc843268
indigoxela <user@olga.indigofloat.at>
2020-10-06 14:44:15 +0200

    Fix strange white space usage

--- core/modules/simpletest/backdrop_web_test_case.php
+++ core/modules/simpletest/backdrop_web_test_case.php
@@ -1630,12 +1630,12 @@ class BackdropWebTestCase extends BackdropTestCase {
    */
   private function recursiveCopy($src, $dst) {
     $dir = opendir($src);
-    if(!file_exists($dst)){
+    if (!file_exists($dst)) {
       mkdir($dst);
     }
-    while(false !== ( $file = readdir($dir)) ) {
+    while (false !== ($file = readdir($dir))) {
       if ($file != '.' && $file != '..' && $file != '.htaccess') {
-        if ( is_dir($src . '/' . $file) ) {
+        if (is_dir($src . '/' . $file)) {
           $this->recursiveCopy($src . '/' . $file, $dst . '/' . $file);
         }
         else {
