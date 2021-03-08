81fdbf2177940f59a9daf89c4ba0311f831fb845
docwilmot <docwilmot@gmail.com>
2021-03-08 17:16:40 -0500

    Add debounce

--- core/misc/announce.js
+++ core/misc/announce.js
@@ -14,66 +14,51 @@
  *     ));
  *   });
  */
-(function (document, Backdrop) {
+(function (Backdrop, debounce) {
+  var liveElement;
+  var announcements = [];
+  Backdrop.behaviors.backdropAnnounce = {
+    attach: function attach(context) {
+      if (!liveElement) {
+        liveElement = document.createElement('div');
+        liveElement.id = 'backdrop-live-announce';
+        liveElement.className = 'visually-hidden';
+        liveElement.setAttribute('aria-live', 'polite');
+        liveElement.setAttribute('aria-busy', 'false');
+        document.body.appendChild(liveElement);
+      }
+    }
+  };
 
-var liveElement;
+  function announce() {
+    var text = [];
+    var priority = 'polite';
+    var announcement;
+    var il = announcements.length;
 
-/**
- * Builds a div element with the aria-live attribute and attaches it
- * to the DOM.
- */
-Backdrop.behaviors.backdropAnnounce = {
-  attach: function (settings, context) {
-    if (!liveElement) {
-      liveElement = document.createElement('div');
-      liveElement.id = 'backdrop-live-announce';
-      liveElement.className = 'element-invisible';
-      liveElement.setAttribute('aria-live', 'polite');
+    for (var i = 0; i < il; i++) {
+      announcement = announcements.pop();
+      text.unshift(announcement.text);
+
+      if (announcement.priority === 'assertive') {
+        priority = 'assertive';
+      }
+    }
+
+    if (text.length) {
+      liveElement.innerHTML = '';
+      liveElement.setAttribute('aria-busy', 'true');
+      liveElement.setAttribute('aria-live', priority);
+      liveElement.innerHTML = text.join('\n');
       liveElement.setAttribute('aria-busy', 'false');
-      document.body.appendChild(liveElement);
     }
   }
-};
 
-/**
- * Triggers audio UAs to read the supplied text.
- *
- * @param {String} text
- *   - A string to be read by the UA.
- *
- * @param {String} priority
- *   - A string to indicate the priority of the message. Can be either
- *   'polite' or 'assertive'. Polite is the default.
- *
- * Use Backdrop.announce to indicate to screen reader users that an element on
- * the page has changed state. For instance, if clicking a link loads 10 more
- * items into a list, one might announce the change like this.
- * $('#search-list')
- *   .on('itemInsert', function (event, data) {
- *     // Insert the new items.
- *     $(data.container.el).append(data.items.el);
- *     // Announce the change to the page contents.
- *     Backdrop.announce(Backdrop.t('@count items added to @container',
- *       {'@count': data.items.length, '@container': data.container.title}
- *     ));
- *   });
- *
- * @see http://www.w3.org/WAI/PF/aria-practices/#liveprops
- */
-Backdrop.announce = function (text, priority) {
-  if (typeof text === 'string') {
-    // Clear the liveElement so that repeated strings will be read.
-    liveElement.innerHTML = '';
-    // Set the busy state to true until the node changes are complete.
-    liveElement.setAttribute('aria-busy', 'true');
-    // Set the priority to assertive, or default to polite.
-    liveElement.setAttribute('aria-live', (priority === 'assertive') ? 'assertive' : 'polite');
-    // Print the text to the live region.
-    liveElement.innerHTML = Backdrop.checkPlain(text);
-    // The live text area is updated. Allow the AT to announce the text.
-    liveElement.setAttribute('aria-busy', 'false');
-  }
-};
-
-}(document, Backdrop));
-
+  Backdrop.announce = function (text, priority) {
+    announcements.push({
+      text: text,
+      priority: priority
+    });
+    return debounce(announce, 300)();
+  };
+})(Backdrop, Backdrop.debounce);
--- core/misc/backdrop.js
+++ core/misc/backdrop.js
@@ -827,4 +827,34 @@ Backdrop.optimizedResize = (function() {
   }
 }());
 
+Backdrop.debounce = function (func, wait, immediate) {
+  var timeout;
+  var result;
+  return function () {
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    var context = this;
+
+    var later = function later() {
+      timeout = null;
+
+      if (!immediate) {
+        result = func.apply(context, args);
+      }
+    };
+
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+
+    if (callNow) {
+      result = func.apply(context, args);
+    }
+
+    return result;
+  };
+};
+
 })(jQuery);
