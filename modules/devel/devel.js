41f587d21f6f8a5e44a3c8fe955d75f321fa307f
Jen Lampton <jen@jeneration.com>
2021-09-20 15:03:30 -0700

    Add devel for generating content to PR sandbox.

---
+++ modules/devel/devel.js
@@ -0,0 +1,62 @@
+/**
+ * @file
+ * Behaviors for Devel.
+ */
+
+(function ($) {
+
+/**
+ * Explain link in query log.
+ *
+ * @type {Backdrop~behavior}
+ */
+Backdrop.behaviors.devel_explain = {
+  attach: function() {
+    $('a.dev-explain').click(function () {
+      qid = $(this).attr("qid");
+      cell = $('#devel-query-' + qid);
+      $('.dev-explain', cell).load(Backdrop.settings.basePath + '?q=devel/explain/' + Backdrop.settings.devel.request_id + '/' + qid).show();
+      $('.dev-placeholders', cell).hide();
+      $('.dev-arguments', cell).hide();
+      return false;
+    });
+  }
+}
+
+/**
+ * Arguments link in query log.
+ *
+ * @type {Backdrop~behavior}
+ */
+Backdrop.behaviors.devel_arguments = {
+  attach: function() {
+    $('a.dev-arguments').click(function () {
+      qid = $(this).attr("qid");
+      cell = $('#devel-query-' + qid);
+      $('.dev-arguments', cell).load(Backdrop.settings.basePath + '?q=devel/arguments/' + Backdrop.settings.devel.request_id + '/' + qid).show();
+      $('.dev-placeholders', cell).hide();
+      $('.dev-explain', cell).hide();
+      return false;
+    });
+  }
+}
+
+/**
+ * Placeholders link in query log.
+ *
+ * @type {Backdrop~behavior}
+ */
+Backdrop.behaviors.devel_placeholders = {
+  attach: function() {
+    $('a.dev-placeholders').click(function () {
+      qid = $(this).attr("qid");
+      cell = $('#devel-query-' + qid);
+      $('.dev-explain', cell).hide();
+      $('.dev-arguments', cell).hide();
+      $('.dev-placeholders', cell).show();
+      return false;
+    });
+  }
+}
+
+})(jQuery);
