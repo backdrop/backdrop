d089aa8589139a9ca22e5c10f65ce5cb7c36d48b
Herb v/d Dool <herb@3speedhub.com>
2021-12-13 12:04:21 -0500

    fix classes_array to classes

--- core/modules/entity/templates/entity.tpl.php
+++ core/modules/entity/templates/entity.tpl.php
@@ -12,16 +12,11 @@
  * - $title: The (sanitized) entity label.
  * - $url: Direct url of the current entity if specified.
  * - $page: Flag for the full page state.
- * - $classes: String of classes that can be used to style contextually through
- *   CSS. It can be manipulated through the variable $classes_array from
- *   preprocess functions. By default the following classes are available, where
- *   the parts enclosed by {} are replaced by the appropriate values:
+ * - Array of html class attribute values. By default the following classes are
+ *   available, where the parts enclosed by {} are replaced by the appropriate
+ *   values:
  *   - entity-{ENTITY_TYPE}
  *   - {ENTITY_TYPE}-{BUNDLE}
- *
- * Other variables:
- * - $classes_array: Array of html class attribute values. It is flattened
- *   into a string within the variable $classes.
  *
  * @see template_preprocess()
  * @see template_preprocess_entity()
@@ -39,7 +34,7 @@
     </h2>
   <?php endif; ?>
 
-  <div class="content"<?php (empty($content_attributes)) ? '' : print backdrop_attributes($content_attributes); ?>>
+  <div class="content clearfix"<?php (empty($content_attributes)) ? '' : print backdrop_attributes($content_attributes); ?>>
     <?php
       print render($content);
     ?>
