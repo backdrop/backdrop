1bc41aacd3f5bc2a98f92defbcc28f3d474889de
Greg Netsas <gregory@userfriendly.tech>
2022-03-06 13:30:43 +0200

    ...combine the docblock for the 2 settings

--- settings.php
+++ settings.php
@@ -59,39 +59,32 @@ $config_directories['staging'] = 'files/config_' . md5($database) . '/staging';
 $settings['update_free_access'] = FALSE;
 
 /**
- * Use HTTP for the Update Manager.
+ * Bypass HTTPS for the Project Installer and/or SSL verification for the Update
+ * Manager.
  *
- * If your Backdrop site fails to connect to updates.backdropcms.org using HTTPS
- * to fetch the update status of Backdrop core and/or contrib projects, you may
- * uncomment this setting (and make sure it is set to "TRUE"), to allow an
- * insecure connection via HTTP.
+ * Two separate settings are provided:
+ * - update_fetch_with_http: Enable this setting if your Backdrop site fails to
+ *     connect to updates.backdropcms.org using HTTPS to fetch the update status
+ *     of Backdrop core and/or contributed projects. You may uncomment ths
+ *     setting (and make sure it is set to "TRUE"), to allow an insecure
+ *     connection via HTTP.
+ * - installer_disable_ssl_verification: Enable this setting if your Backdrop
+ *     site fails to connect to projects.backdropcms.org to fetch the list of
+ *     contributed projects, you may uncomment this setting (and make sure it is
+ *     set to "TRUE"), to allow an insecure connection without SSL verification.
  *
- * SECURITY WARNING: Enabling this setting will open your site up to potential
- * man-in-the-middle attacks! You should instead attempt to resolve the issues,
- * and only use this option as a last resort.
+ * SECURITY WARNING!: Enabling any of these two settings will open your site up
+ * to potential man-in-the-middle attacks! It is strongly advised that you
+ * instead attempt to resolve underlying SSL issues, and only use this option as
+ * a last resort.
  *
  * @see https://docs.backdropcms.org/documentation/system-requirements#php
  * @see https://en.wikipedia.org/wiki/Man-in-the-middle_attack
  * @see _update_build_fetch_url()
- */
-# $settings['update_fetch_with_http'] = TRUE;
-
-/**
- * Disable SSL verification for the Project Installer.
- *
- * If your Backdrop site fails to connect to projects.backdropcms.org to fetch
- * a list of contrib projects, you may uncomment this setting (and make sure it
- * is set to "TRUE"), to allow an insecure connection without SSL verification.
- *
- * SECURITY WARNING: Enabling this setting will open your site up to potential
- * man-in-the-middle attacks! You should instead attempt to resolve the issues,
- * and only use this option as a last resort.
- *
- * @see https://docs.backdropcms.org/documentation/system-requirements#php
- * @see https://en.wikipedia.org/wiki/Man-in-the-middle_attack
  * @see installer_browser_fetch_results()
  */
-# $settings['installer_disable_ssl_verification'] = TRUE;
+// $settings['update_fetch_with_http'] = TRUE;
+// $settings['installer_disable_ssl_verification'] = TRUE;
 
 /**
  * Salt for one-time login links and cancel links, form tokens, etc.
