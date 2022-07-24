215df2262fd1d7d56b4bce27610247b849fabdad
indigoxela <user@olga.indigofloat.at>
2022-07-24 12:02:22 +0200

    Completely new approach

--- test.php
+++ test.php
@@ -17,3 +17,17 @@ function foo() {
   // Another failure.
   $fooMyVariable = 'camelcase';
 }
+
+/**
+ * None of the above still causes phpcs to nag, we have to be sloppier, but nagging still happens. Not with comments, though.
+ */
+function bar($one, $two = 'what', $three) {
+    $test = ''; 
+  switch ($two) {
+    case 'a':
+      $test= 'space';
+    case 'b':
+      $test ='nobreak';
+  }
+  $newvar = isset($three)?$three : null;
+}
