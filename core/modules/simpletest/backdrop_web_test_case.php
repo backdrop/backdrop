122bed159f23fcc61676599c17b365f8d0b08ff1
hosef <joseph@flattandsons.com>
2022-10-20 11:35:39 -0700

    Issue #5799: Move the code that changes the public files into the middle of the code that installs backdrop.

--- core/modules/simpletest/backdrop_web_test_case.php
+++ core/modules/simpletest/backdrop_web_test_case.php
@@ -1700,20 +1700,19 @@ class BackdropWebTestCase extends BackdropTestCase {
     config_install_default_config('system');
     config_set('system.core', 'install_profile', $this->profile);
 
-    // Set path variables.
-    config_install_default_config('system.core');
-    $core_config = config('system.core');
-    $core_config->set('file_default_scheme', 'public');
-    $core_config->set('file_public_path', $this->public_files_directory);
-    $core_config->set('file_private_path', $this->private_files_directory);
-    $core_config->set('file_temporary_path', $this->temp_files_directory);
-    $core_config->save();
-
     $use_cache = $this->useCache();
     if (!$use_cache) {
       // Perform the actual Backdrop installation.
       include_once BACKDROP_ROOT . '/core/includes/install.inc';
       backdrop_install_system();
+
+      // Set path variables.
+      $core_config = config('system.core');
+      $core_config->set('file_default_scheme', 'public');
+      $core_config->set('file_public_path', $this->public_files_directory);
+      $core_config->set('file_private_path', $this->private_files_directory);
+      $core_config->set('file_temporary_path', $this->temp_files_directory);
+      $core_config->save();
 
       // Ensure schema versions are recalculated.
       backdrop_static_reset('backdrop_get_schema_versions');
@@ -1730,6 +1729,16 @@ class BackdropWebTestCase extends BackdropTestCase {
         module_enable(array($this->profile), FALSE);
       }
     }
+    else {
+      // Set path variables. This must be done even when using a profile cache.
+      $core_config = config('system.core');
+      $core_config->set('file_default_scheme', 'public');
+      $core_config->set('file_public_path', $this->public_files_directory);
+      $core_config->set('file_private_path', $this->private_files_directory);
+      $core_config->set('file_temporary_path', $this->temp_files_directory);
+      $core_config->save();
+    }
+
 
     // Set 'parent_profile' of simpletest to add the parent profile's
     // search path to the child site's search paths.
