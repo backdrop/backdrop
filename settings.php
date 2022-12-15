21c8a2fa7134b3dca6fcc5ebeef8cd941a76b854
kiamlaluno <kiamlaluno@isepro.org>
2022-12-15 09:35:01 +0100

    Issue #5722 by kiamlaluno: Update the links pointing to php.net

--- settings.php
+++ settings.php
@@ -153,7 +153,7 @@ $settings['hash_salt'] = '';
  *
  * To see what PHP settings are possible, including whether they can be set at
  * runtime (by using ini_set()), read the PHP documentation:
- * http://www.php.net/manual/ini.list.php
+ * https://www.php.net/manual/ini.list.php
  * See backdrop_environment_initialize() in includes/bootstrap.inc for required
  * runtime settings and the .htaccess file for non-runtime settings. Settings
  * defined there should not be duplicated here so as to avoid conflict issues.
@@ -189,7 +189,7 @@ ini_set('session.cookie_lifetime', 2000000);
  * output filter may not have sufficient memory to process it.  If you
  * experience this issue, you may wish to uncomment the following two lines
  * and increase the limits of these variables.  For more information, see
- * http://php.net/manual/en/pcre.configuration.php.
+ * https://www.php.net/manual/pcre.configuration.php.
  */
 // ini_set('pcre.backtrack_limit', 200000);
 // ini_set('pcre.recursion_limit', 200000);
