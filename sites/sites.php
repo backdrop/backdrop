6ebaee7d8443b8b5ff82f143be632fad3b9a3aeb
BWPanda <BWPanda@users.noreply.github.com>
2023-02-24 12:35:15 +1100

    Add bug.

--- sites/sites.php
+++ sites/sites.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @file
- * Configuration file for the Backdrop CMS multi-site feature.
+ * Configuration file for the Backdrop CMS multi-site feature..
  *
  * This file allows you to map hostnames, ports, and pathnames to configuration
  * directories. Information on how to set up multisites see the README.md file
