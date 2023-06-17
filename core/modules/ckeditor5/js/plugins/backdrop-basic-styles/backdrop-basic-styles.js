e96be9e2b9347a0e0ca1696f48bfe63467108728
quicksketch <nate@quicksketch.org>
2023-06-16 17:57:02 -0700

    Early CKEditor 5 work to get images uploading. Fix table support.

--- core/modules/ckeditor5/js/plugins/backdrop-basic-styles/backdrop-basic-styles.js
+++ core/modules/ckeditor5/js/plugins/backdrop-basic-styles/backdrop-basic-styles.js
@@ -15,21 +15,23 @@ class BackdropBasicStyles extends CKEditor5.core.Plugin {
    * @inheritdoc
    */
   init() {
+    const editor = this.editor;
+
     // CKEditor prefers <i> but Backdrop prefers <em>.
-    this.editor.conversion.for('downcast').attributeToElement({
+    editor.conversion.for('downcast').attributeToElement({
       model: 'italic',
       view: 'em',
       converterPriority: 'high',
     });
     // CKEditor prefers <s> but Backdrop prefers <del>.
-    this.editor.conversion.for('downcast').attributeToElement({
+    editor.conversion.for('downcast').attributeToElement({
       model: 'strikethrough',
       view: 'del',
       converterPriority: 'high',
     });
     // Backdrop previously preferred <span class="underline">. This converts it
     // to <u> tags preferred by CKEditor 5.
-    this.editor.conversion.for('upcast').elementToAttribute({
+    editor.conversion.for('upcast').elementToAttribute({
       model: 'underline',
       view: {
         name: 'span',
@@ -40,9 +42,9 @@ class BackdropBasicStyles extends CKEditor5.core.Plugin {
 
     // Support a minimum height option on the editor.
     // See https://stackoverflow.com/a/56550285/845793
-    const minHeight = this.editor.config.get('minHeight');
+    const minHeight = editor.config.get('minHeight');
     if (minHeight) {
-      this.editor.ui.view.editable.extendTemplate({
+      editor.ui.view.editable.extendTemplate({
         attributes: {
           style: {
             minHeight: minHeight,
@@ -53,6 +55,7 @@ class BackdropBasicStyles extends CKEditor5.core.Plugin {
   }
 }
 
+// Expose the plugin to the CKEditor5 namespace.
 CKEditor5.backdropBasicStyles = {
   'BackdropBasicStyles': BackdropBasicStyles
 };
