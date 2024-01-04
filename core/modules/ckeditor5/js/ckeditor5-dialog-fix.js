a90263873175d81f30e40e734fcc1a87ccbcc615
quicksketch <nate@quicksketch.org>
2024-01-04 14:38:40 -0800

    Expanding documentation.

--- core/modules/ckeditor5/js/ckeditor5-dialog-fix.js
+++ core/modules/ckeditor5/js/ckeditor5-dialog-fix.js
@@ -6,10 +6,18 @@
 
   'use strict';
 
+  /**
+   * Override the _allowInteraction() extension point.
+   *
+   * @see https://api.jqueryui.com/dialog/#method-_allowInteraction
+   * @see https://bugs.jqueryui.com/ticket/9087/
+   */
   $.widget('ui.dialog', $.ui.dialog, {
     _allowInteraction: function _allowInteraction(event) {
-      // All editor form elements, like buttons in balloon toolbars get the "ck"
-      // class.
+      // The CKEditor 5 balloon toolbar is outside the modal container, by
+      // specifying CKEditor elements as allowed-interaction outside the modal,
+      // the balloon buttons can be clicked. All editor form elements, like
+      // buttons in balloon toolbars get the "ck" class.
       if ($(event.target).hasClass('ck')) {
         return true;
       }
