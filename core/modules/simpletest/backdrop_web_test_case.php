1c4c509a8fdb14d1f6bbfec96b8089bde967625f
Robert Garrigos <robert@garrigos.cat>
2024-11-08 16:53:12 +0100

    revised message

--- core/modules/simpletest/backdrop_web_test_case.php
+++ core/modules/simpletest/backdrop_web_test_case.php
@@ -1348,7 +1348,7 @@ class BackdropWebTestCase extends BackdropTestCase {
     }
 
     // Check that all the permission strings are valid.
-    // @todo: Remove 'TRUE' when we sort out https://github.com/backdrop/backdrop-issues/issues/5354
+    // @todo: Remove the TRUE parameter when we sort out https://github.com/backdrop/backdrop-issues/issues/5354
     if (!$this->checkPermissions($permissions, TRUE)) {
       return FALSE;
     }
