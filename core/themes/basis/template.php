88f50a9454db1610254a7cc91c0fdb053f7ebd15
Herb v/d Dool <herb@3speedhub.com>
2024-12-16 13:18:37 -0500

    Issue #6778: Move css files to hook_css_alter()

--- core/themes/basis/template.php
+++ core/themes/basis/template.php
@@ -8,11 +8,24 @@
  * Implements hook_css_alter().
  */
 function basis_css_alter(&$css) {
-  // Remove Basis' `/css/component/menu-dropdown.css` if using a custom
-  // breakpoint.
-  if (config_get('menu.settings', 'menu_breakpoint') == 'custom') {
+  // Remove Basis' `/css/component/menu-dropdown.css` and add breakpoint files
+  // if using a custom breakpoint.
+  $config = config('menu.settings');
+  if ($config->get('menu_breakpoint') == 'custom') {
     $path = backdrop_get_path('theme', 'basis');
+    $weight = isset($css[$path . '/css/component/menu-dropdown.css']['weight']) ? $css[$path . '/css/component/menu-dropdown.css']['weight'] : 0;
     unset($css[$path . '/css/component/menu-dropdown.css']);
+
+    $weight += 0.0001;
+    $css[$path . '/css/component/menu-dropdown.breakpoint.css'] = $css[$path . '/css/base.css'];
+    $css[$path . '/css/component/menu-dropdown.breakpoint.css']['weight'] = $weight;
+    $css[$path . '/css/component/menu-dropdown.breakpoint.css']['data'] = $path . '/css/component/menu-dropdown.breakpoint.css';
+
+    $weight += 0.0001;
+    $css[$path . '/css/component/menu-dropdown.breakpoint-queries.css'] = $css[$path . '/css/base.css'];
+    $css[$path . '/css/component/menu-dropdown.breakpoint-queries.css']['weight'] = $weight;
+    $css[$path . '/css/component/menu-dropdown.breakpoint-queries.css']['media'] = 'all and (min-width: ' . $config->get('menu_breakpoint_custom') . ')';
+    $css[$path . '/css/component/menu-dropdown.breakpoint-queries.css']['data'] = $path . '/css/component/menu-dropdown.breakpoint-queries.css';
   }
 }
 
@@ -36,20 +49,6 @@ function basis_preprocess_page(&$variables) {
   $view = views_get_page_view();
   if ($view) {
     $variables['classes'][] = 'view-name-' . $view->name;
-  }
-
-  // Add breakpoint-specific CSS for dropdown menus.
-  $config = config('menu.settings');
-  if ($config->get('menu_breakpoint') == 'custom') {
-    backdrop_add_css(backdrop_get_path('theme', 'basis') . '/css/component/menu-dropdown.breakpoint.css', array(
-      'group' => CSS_THEME,
-      'every_page' => TRUE,
-    ));
-    backdrop_add_css(backdrop_get_path('theme', 'basis') . '/css/component/menu-dropdown.breakpoint-queries.css', array(
-      'group' => CSS_THEME,
-      'every_page' => TRUE,
-      'media' => 'all and (min-width: ' . $config->get('menu_breakpoint_custom') . ')',
-    ));
   }
 }
 
