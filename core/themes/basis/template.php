09ae4c60239f243f68c88c75bed05d844b652145
Herb <herb@3speedhub.com>
2024-12-17 13:38:23 -0500

    Fix indentation

--- core/themes/basis/template.php
+++ core/themes/basis/template.php
@@ -17,12 +17,12 @@ function basis_css_alter(&$css) {
     unset($css[$path . '/css/component/menu-dropdown.css']);
 
     $weight += 0.0001;
-$css[$path . '/css/component/menu-dropdown.breakpoint.css'] = $dropdown_css;
+    $css[$path . '/css/component/menu-dropdown.breakpoint.css'] = $dropdown_css;
     $css[$path . '/css/component/menu-dropdown.breakpoint.css']['weight'] = $weight;
     $css[$path . '/css/component/menu-dropdown.breakpoint.css']['data'] = $path . '/css/component/menu-dropdown.breakpoint.css';
 
     $weight += 0.0001;
-$css[$path . '/css/component/menu-dropdown.breakpoint-queries.css'] = $dropdown_css;
+    $css[$path . '/css/component/menu-dropdown.breakpoint-queries.css'] = $dropdown_css;
     $css[$path . '/css/component/menu-dropdown.breakpoint-queries.css']['weight'] = $weight;
     $css[$path . '/css/component/menu-dropdown.breakpoint-queries.css']['media'] = 'all and (min-width: ' . $config->get('menu_breakpoint_custom') . ')';
     $css[$path . '/css/component/menu-dropdown.breakpoint-queries.css']['data'] = $path . '/css/component/menu-dropdown.breakpoint-queries.css';
