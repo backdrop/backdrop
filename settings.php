7be0dbe55d04621cc20a530b8cc62e36a8a73ec5
laryn <laryn@kragtbakker.com>
2025-01-05 18:16:31 -0600

    Remove dev elements accidentally committed.

--- settings.php
+++ settings.php
@@ -578,9 +578,3 @@ $settings['backdrop_drupal_compatibility'] = TRUE;
 if (file_exists(__DIR__ . '/settings.local.php')) {
   include __DIR__ . '/settings.local.php';
 }
-
-// Automatically generated include for settings managed by ddev.
-$ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
-if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
-  require $ddev_settings;
-}
