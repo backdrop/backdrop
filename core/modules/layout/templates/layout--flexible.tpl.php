2390c160db9a8b61864785c79d139c08c21951dc
Laryn Kragt Bakker <laryn@kragtbakker.com>
2025-04-18 13:09:05 -0500

    Tweaks whiel working on Layout Paragraphs.

--- core/modules/layout/templates/layout--flexible.tpl.php
+++ core/modules/layout/templates/layout--flexible.tpl.php
@@ -23,9 +23,11 @@
  */
 ?>
 <div class="layout--flexible layout <?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
-  <div id="skip-link">
-    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
-  </div>
+  <?php if ($is_full_page): ?>
+    <div id="skip-link">
+      <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+    </div>
+  <?php endif; ?>
   <div class="layout-flexible-content <?php $region_buttons ? print 'layout-flexible-editor' : ''; ?>">
     <?php foreach ($row_data as $name => $row): ?>
       <?php
