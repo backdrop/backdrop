b9053c11048fa7ff4314f9e0d6f92e8ff213fc0b
quicksketch <nate@quicksketch.org>
2025-06-10 00:56:00 -0700

    Issue #6904: Expand documentation for ckeditor5-prepare.js.

--- core/modules/ckeditor5/js/ckeditor5-prepare.js
+++ core/modules/ckeditor5/js/ckeditor5-prepare.js
@@ -2,22 +2,43 @@
  * @file
  * Backwards compatibility of UMD build to integration via DLL builds.
  */
-(function () {
+(function (CKEDITOR) {
   "use strict";
 
-  // Mimic some of the DLL structure by adding some aliases for commonly
-  // extended classes. It won't be possible to mimic the entire DLL structure.
-  // This must happen before any custom plugin js files load.
-  CKEDITOR.core = {};// Does not exist in UMD.
-  CKEDITOR.core.Plugin = CKEDITOR.Plugin;
-  CKEDITOR.core.Command = CKEDITOR.Command;
+  /**
+   * Prepare CKEditor 5 namespace variables for backwards-compatibility.
+   *
+   * CKEditor 5 versions 45 and higher stopped supporting the loading mechanism
+   * Backdrop used previously (DLL builds) and changed the namespace used by
+   * the editor from "CKEditor5" to "CKEDITOR".
+   *
+   * This mimics some of the DLL structure by adding aliases for commonly
+   * extended classes. This does not mimic the entire DLL structure, only the
+   * ones most likely to affect contributed modules. This must happen before any
+   * custom plugin JS files load.
+   */
+  function prepareCKEditorNamespaces() {
+    /**
+     * @deprecated "CKEDITOR.core" does not exist in UMD builds.
+     */
+    CKEDITOR.core = {};
+    CKEDITOR.core.Plugin = CKEDITOR.Plugin;
+    CKEDITOR.core.Command = CKEDITOR.Command;
 
-  CKEDITOR.ui = {};// Does not exist in UMD.
-  CKEDITOR.ui.ButtonView = CKEDITOR.ButtonView;
+    /**
+     * @deprecated "CKEDITOR.ui" does not exist in UMD builds.
+     */
+    CKEDITOR.ui = {};
+    CKEDITOR.ui.ButtonView = CKEDITOR.ButtonView;
 
-  // Use a different name to expose to global space - the one we used before.
-  window.CKEditor5 = CKEDITOR;
-  // Prevent conflict with CKE4, depends on order, v4 has to run after this.
-  delete window.CKEDITOR;
+    // Use a different name to expose to global space - the one we used before.
+    window.CKEditor5 = CKEDITOR;
 
-})();
+    // Prevent conflict with CKEditor 4. Note this renaming depends on the
+    // loading order, v4 has to run after this file.
+    delete window.CKEDITOR;
+  }
+
+  prepareCKEditorNamespaces();
+
+})(window.CKEDITOR);
