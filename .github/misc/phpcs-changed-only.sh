6a509be1e9fb060c04464d7b57940565c05df27e
indigoxela <user@olga.indigofloat.at>
2025-09-30 15:55:54 +0200

    Modified approach to allow a lot more files

--- .github/misc/phpcs-changed-only.sh
+++ .github/misc/phpcs-changed-only.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Get the list of relevant changed files compared to 1.x and run phpcs on them.
 
-FILES=$(git diff origin/1.x --name-only --diff-filter=AM | grep -E '(php|inc|module|install|profile|engine|test)$' | grep -v '^.github')
-# debug:
-echo $FILES
+TMPFILE=$(mktemp /tmp/filelist-XXXXXXXX)
+git diff origin/1.x --name-only --diff-filter=AM | grep -E '(php|inc|module|install|profile|engine|test)$' | grep -v '^.github' > $TMPFILE
 
-phpcs -nq --basepath=. --standard=../phpcs/Backdrop --report=.github/misc/Github.php $FILES
+phpcs -nq --basepath=. --standard=../phpcs/Backdrop --report=.github/misc/Github.php --file-list=$TMPFILE
+
+rm $TMPFILE
