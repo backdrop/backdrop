c0eddf600385e59c4027bfc65d0e03ed9e282f48
indigoxela <user@olga.indigofloat.at>
2025-09-30 15:10:25 +0200

    Issue #6973: Replace phpcs action

---
+++ .github/misc/phpcs-changed-only.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Get the list of relevant changed files compared to 1.x and run phpcs on them.
+
+FILES=$(git diff origin/1.x --name-only --diff-filter=AM | grep -E '(php|inc|module|install|profile|engine|test)$')
+
+echo $FILES | while read FILENAME
+do
+  phpcs -nq --basepath=. --standard=../phpcs/Backdrop --report=json $FILENAME
+done
+
+exit 0
