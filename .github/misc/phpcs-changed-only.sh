c45042f5da3f1a233996996b74496afc1f42c82b
indigoxela <user@olga.indigofloat.at>
2025-09-30 15:38:32 +0200

    Modified approach

--- .github/misc/phpcs-changed-only.sh
+++ .github/misc/phpcs-changed-only.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # Get the list of relevant changed files compared to 1.x and run phpcs on them.
 
-FILES=$(git diff origin/1.x --name-only --diff-filter=AM | grep -E '(php|inc|module|install|profile|engine|test)$')
+FILES=$(git diff origin/1.x --name-only --diff-filter=AM | grep -E '(php|inc|module|install|profile|engine|test)$' | grep -v '^.github')
+# debug:
+echo $FILES
 
-echo $FILES | while read FILENAME
-do
-  phpcs -nq --basepath=. --standard=../phpcs/Backdrop --report=.github/misc/Github.php $FILENAME
-done
+phpcs -nq --basepath=. --standard=../phpcs/Backdrop --report=.github/misc/Github.php $FILES
