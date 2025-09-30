091875ae31c67b9508c0444c8e6638e27ff2c01c
indigoxela <user@olga.indigofloat.at>
2025-09-30 16:04:06 +0200

    Struggling with exit codes

--- .github/misc/phpcs-changed-only.sh
+++ .github/misc/phpcs-changed-only.sh
@@ -5,5 +5,9 @@ TMPFILE=$(mktemp /tmp/filelist-XXXXXXXX)
 git diff origin/1.x --name-only --diff-filter=AM | grep -E '(php|inc|module|install|profile|engine|test)$' | grep -v '^.github' > $TMPFILE
 
 phpcs -nq --basepath=. --standard=../phpcs/Backdrop --report=.github/misc/Github.php --file-list=$TMPFILE
+FINALEXIT=$?
 
 rm $TMPFILE
+# Otherwise we get the exit code from rm?
+echo $FINALEXIT
+exit $FINALEXIT
