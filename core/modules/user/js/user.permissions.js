bf68accc0867e0dd9de5d7d413251e077424a729
indigoxela <user@olga.indigofloat.at>
2025-11-04 10:33:32 +0100

    Add same comment in User module js code

--- core/modules/user/js/user.permissions.js
+++ core/modules/user/js/user.permissions.js
@@ -61,6 +61,8 @@ Backdrop.behaviors.permissionsFilter = {
     }
 
     if ($form.length) {
+      // Set focus on search input only, if no hash requires jumping to a
+      // specific item on page.
       if (window.location.hash === '') {
         $input.trigger('focus');
       }
