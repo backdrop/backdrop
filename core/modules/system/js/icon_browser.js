53f4d29b7798ed56de31501f8e132dc8aa14aeb6
docwilmot <docwilmot@users.noreply.github.com>
2026-04-11 22:38:06 -0500

    Start adding click-insert code

--- core/modules/system/js/icon_browser.js
+++ core/modules/system/js/icon_browser.js
@@ -8,25 +8,99 @@
    * to details as well.
    */
   Backdrop.behaviors.iconBrowser = {
-    attach: function (context, settings) {
-      const iconList = document.querySelectorAll(".icon-browser-list .icon-wrapper");
-      // Indicate which icons are active.
-      iconList.forEach((iconItem) => {
-        var iconName = iconItem.getAttribute('data-icon-name');
-        if (iconName && Backdrop.icons[iconName]) {
-          iconItem.setAttribute('data-icon-active', true);
-        }
-      });
-      const iconDetail = document.querySelector(".icon-browser-detail");
-      if (iconDetail) {
-        const iconStatus = document.querySelector(".icon-browser-detail .icon-status span");
-        var iconName = iconDetail.getAttribute('data-icon-name');
-        if (iconName && Backdrop.icons[iconName]) {
-          iconDetail.setAttribute('data-icon-active', true);
-          iconStatus.innerHTML = Backdrop.t("Active");
-        }
+  attach: function (context, settings) {
+
+    /**
+     * Selects a token.
+     * 
+     * Invoked when a token link is clicked.
+     */
+    function select(event) {
+      var $token = $(event.currentTarget );
+
+      event.preventDefault();
+      event.stopPropagation();
+
+      if (Backdrop.settings.iconBrowserSelectedToken) {
+        Backdrop.settings.iconBrowserSelectedToken.removeClass('selected-token');
+        Backdrop.settings.iconBrowserSelectedToken.removeAttr('data-after');
+        Backdrop.settings.iconBrowserSelectedToken.removeAttr('aria-selected');
+      }
+
+      if (Backdrop.settings.iconBrowserSelectedToken && $token[0] === Backdrop.settings.iconBrowserSelectedToken[0]) {
+        Backdrop.settings.iconBrowserSelectedToken = null;
+      }
+      else {
+        console.log('kik');
+        Backdrop.settings.iconBrowserSelectedToken = $token;
+        Backdrop.settings.iconBrowserSelectedToken.addClass('selected-token');
+        Backdrop.settings.iconBrowserSelectedToken.attr('data-after', ' (' + Backdrop.t('selected') + ')');
+        Backdrop.settings.iconBrowserSelectedToken.attr('aria-selected');
+      }
+      // If we have a focused field, insert the selected token.
+      if (typeof Backdrop.settings.iconBrowserFocusedField !== 'undefined' && Backdrop.settings.iconBrowserSelectedToken) {
+      console.log(Backdrop.settings.iconBrowserSelectedToken);
+        insert(Backdrop.settings.iconBrowserFocusedField);
       }
     }
-  };
 
+    /**
+     * Inserts a token in a DOM text field.
+     * 
+     * @param myField 
+     */
+    function insert(myField) {
+      if (Backdrop.settings.iconBrowserSelectedToken) {
+        var startPos = myField.selectionStart;
+        var endPos = myField.selectionEnd;
+        var myValue  = Backdrop.settings.iconBrowserSelectedToken;
+        var myValue  = $(myValue).data('icon-name');
+        myField.value = 
+          myField.value.substring(0, startPos)
+          + myValue
+          + myField.value.substring(endPos, myField.value.length)
+        ;
+        Backdrop.settings.iconBrowserSelectedToken.removeClass('selected-token');
+        Backdrop.settings.iconBrowserSelectedToken.removeAttr('aria-selected');
+        Backdrop.settings.iconBrowserSelectedToken = null;
+      }
+    }
+
+    var click_insert = Backdrop.settings.iconBrowser.click_insert;
+
+    // tr.setAttribute('aria-level', level);
+    // tr.setAttribute('aria-posinset', index);
+
+    // $('.icon-wrapper').addEventListener('click', expand);
+
+    if (click_insert) {
+      $('.icon-wrapper').attr('title', 'Select the token. Click in a text field to insert it.');
+      $('.icon-wrapper').on('click', select);
+    }
+
+    // Set up listener for the token tree button.
+    // $(context).find('button').on('click', expand);
+
+    // Keep track of which textfield was last selected/focused.
+    $('.page').find('textarea, input[type="text"]').once('token-browser-field-focus').on('focus', function() {
+      console.log('Backdrop.settings.iconBrowserSelectedToken');
+      Backdrop.settings.iconBrowserFocusedField = this;
+    });
+
+    // Finds a textarea where token will be inserted and starts a listener.
+    var $input = $('textarea, input[type="text"]', context);
+    if ($input.length) {
+      $input.once('token-browser-insert').on('click', function (event) {
+        insert(event.target);
+      });
+    }
+
+    // Attach listener to open first token type when there is only one in the table.
+    $('body').once('token-browser-dialogopen').on('dialogopen', '.token-browser-dialog', function (event){
+      if ($(this).find('table tr.tree-grid-parent').length == 1) {
+        $(this).find('table tr.tree-grid-parent').first().find('button').trigger('click');
+      }
+    });
+  }
+}
 })(jQuery);
