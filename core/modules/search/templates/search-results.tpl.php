c84a04c7db08e89de57580a9ad8d3f2f5d3dd4c5
izmeez <6208775+izmeez@users.noreply.github.com>
2026-06-26 21:30:05 -0700

    Fixed search results numbering across pages.

--- core/modules/search/templates/search-results.tpl.php
+++ core/modules/search/templates/search-results.tpl.php
@@ -14,17 +14,20 @@
  * - $search_results: Array of data for each result
  * - $module: The machine-readable name of the module (tab) being searched, such
  *   as "node" or "user".
+ * - $ol_start: The starting number for the ordered list, calculated from the
+ *   current pager page so numbering is sequential across pages.
  *
+ * @since 1.34.3 Added ol_start attribute.
  *
  * @see template_preprocess_search_results()
  *
  * @ingroup themeable
  */
 ?>
-<?php if (!empty($search_results)): ?>
-  <h2><?php print t('Search results');?></h2>
-  <ol class="search-results <?php print $module; ?>-results">
-    <?php foreach ($search_results as $result): ?>
+<?php if (!empty($search_results)) : ?>
+  <h2><?php print t('Search results'); ?></h2>
+  <ol class="search-results <?php print $module; ?>-results" start="<?php print $ol_start; ?>">
+    <?php foreach ($search_results as $result) : ?>
       <li <?php print $result['attributes']; ?>>
         <?php print render($result['title_prefix']); ?>
         <h3 class="title">
@@ -32,10 +35,10 @@
         </h3>
         <?php print render($result['title_suffix']); ?>
         <div class="search-snippet-info">
-          <?php if ($result['snippet']): ?>
+          <?php if ($result['snippet']) : ?>
             <div class="search-snippet"><?php print $result['snippet']; ?></div>
           <?php endif; ?>
-          <?php if ($result['info']): ?>
+          <?php if ($result['info']) : ?>
             <p class="search-info"><?php print $result['info']; ?></p>
           <?php endif; ?>
         </div>
@@ -44,7 +47,7 @@
   </ol>
   <?php print $pager; ?>
 <?php else : ?>
-  <h2><?php print t('Your search yielded no results');?></h2>
+  <h2><?php print t('Your search yielded no results'); ?></h2>
   <ul>
     <li><?php print t('Check if your spelling is correct.'); ?></li>
     <li><?php print t('Remove quotes around phrases to search for each word individually. <em>bike shed</em> will often show more results than <em>&quot;bike shed&quot;</em>.'); ?></li>
